9c3b250a52733d1e1494a6d4da6f747593d58602
Nicolas Bock <nicolasbock@gmail.com>
2010-04-15 06:36:14 -0600

    Tree with variable child matrix on each level.

---
+++ spammpack-C/LinearAlgebraLibrary/src/spamm_print_tree.c
@@ -0,0 +1,35 @@
+#include "spamm.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+spamm_print_tree_node (const struct spamm_node_t *node)
+{
+  int i, j;
+
+  spamm_print_node(node);
+  if (node->child != NULL)
+  {
+    for (i = 0; i < node->M_child; ++i) {
+      for (j = 0; j < node->N_child; ++j)
+      {
+        spamm_print_tree_node(node->child[spamm_dense_index(i, j, node->N_child)]);
+      }
+    }
+  }
+}
+
+void
+spamm_print_tree (const struct spamm_t *A)
+{
+  printf("A: M = %i, N = %i, ", A->M, A->N);
+  printf("M_padded = %i, N_padded = %i, ", A->M_padded, A->N_padded);
+  printf("M_block = %i, N_block = %i, ", A->M_block, A->N_block);
+  printf("M_child = %i, N_child = %i, ", A->M_child, A->N_child);
+  printf("root = %p\n", (void*) A->root);
+
+  if (A->root != NULL)
+  {
+    spamm_print_tree_node(A->root);
+  }
+}
--- spammpack-C/LinearAlgebraLibrary/tests/create_spamm.c
+++ spammpack-C/LinearAlgebraLibrary/tests/create_spamm.c
@@ -5,41 +5,56 @@
 int
 main ()
 {
-  int M = 1000;
-  int N = 1000;
+  int max_size = 4;
+  int M[4] = { 2, 10, 1000, 5000 };
+  int N[4] = { 2, 10, 1000, 5000};
 
-  int M_block[10] = { 1, 1, 1, 2, 2, 2, 5, 8, 8, 10 };
-  int N_block[10] = { 1, 2, 3, 1, 2, 5, 2, 8, 2, 10 };
+  int max_block = 11;
+  int M_block[11] = { 1, 1, 1, 2, 2, 2, 5, 8, 8, 10, 100 };
+  int N_block[11] = { 1, 2, 3, 1, 2, 5, 2, 8, 2, 10, 100 };
 
-  int i, j, k;
+  int max_child = 3;
+  int M_child[3] = { 2, 3, 4 };
+  int N_child[3] = { 2, 3, 4 };
+
+  int i, j, k, l, m;
   double *A_dense;
   struct spamm_t A;
 
-  A_dense = (double*) malloc(sizeof(double)*M*N);
-  for (i = 0; i < M; ++i) {
-    for (j = 0; j < N; ++j)
-    {
-      A_dense[spamm_dense_index(i, j, N)] = rand()/(double) RAND_MAX;
-    }
-  }
-
-  for (k = 0; k < 10; ++k)
+  for (k = 0; k < max_size; ++k)
   {
-    spamm_new(M, N, M_block[k], N_block[k], &A);
-    spamm_dense_to_spamm(M, N, M_block[k], N_block[k], A_dense, &A);
-
-    for (i = 0; i < M; ++i) {
-      for (j = 0; j < N; ++j)
+    A_dense = (double*) malloc(sizeof(double)*M[k]*N[k]);
+    for (i = 0; i < M[k]; ++i) {
+      for (j = 0; j < N[k]; ++j)
       {
-        if (A_dense[spamm_dense_index(i, j, N)] != spamm_get(i, j, &A))
-        {
-          printf("mismatch: A_dense[%i][%i] = %f != A[%i][%i] = %f\n", i, j, A_dense[spamm_dense_index(i, j, N)], i, j, spamm_get(i, j, &A));
-          exit(1);
-        }
+        A_dense[spamm_dense_index(i, j, N[k])] = rand()/(double) RAND_MAX;
       }
     }
 
-    spamm_delete(&A);
+    for (l = 0; l < max_block; ++l)
+    {
+      for (m = 0; m < max_child; ++m)
+      {
+        spamm_new(M[k], N[k], M_block[l], N_block[l], M_child[m], N_child[m], &A);
+        printf("%ix%i matrix, padded %ix%i, block dimensions %ix%i, child dimensions %ix%i\n", M[k], N[k], A.M_padded, A.N_padded, M_block[l], N_block[l], M_child[m], N_child[m]);
+        spamm_dense_to_spamm(M[k], N[k], M_block[l], N_block[l], M_child[m], N_child[m], A_dense, &A);
+
+        for (i = 0; i < M[k]; ++i) {
+          for (j = 0; j < N[k]; ++j)
+          {
+            if (A_dense[spamm_dense_index(i, j, N[k])] != spamm_get(i, j, &A))
+            {
+              printf("mismatch: (A_dense[%i][%i] = %e) != (A[%i][%i] = %e)\n", i, j, A_dense[spamm_dense_index(i, j, N[k])], i, j, spamm_get(i, j, &A));
+              exit(1);
+            }
+          }
+        }
+
+        spamm_delete(&A);
+      }
+    }
+
+    free(A_dense);
   }
 
   fprintf(stderr, "test ok\n");
