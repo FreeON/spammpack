6ca7bef28efa80727aba3245b1abc262ab9d9ee5
Matt Challacombe <matt.challacombe@gmail.com>
2010-09-10 20:51:09 -0600

    Broken still for smaller thresholds.
    Seems like bookeeping issues still. Still, this is progress!
    
    mchalla@griffy:~/Desktop/RESEARCH/SpAMM$ ./a.out H8 8 8 1D-3
     1,    4.00000,       4.00061055,    3.34394,    0.65606,    0.65728
     2,    4.00000,       3.34394373,    2.74149,    1.25851,    0.05360
     3,    4.00000,       3.94639889,    3.76743,    0.23257,    0.12536
     4,    4.00000,       4.12536462,    3.99713,    0.00287,    0.25360
     5,    4.00000,       3.99712625,    3.98596,    0.01404,    0.00829
     6,    4.00000,       4.00828850,    3.99999,    0.00001,    0.01658
     7,    4.00000,       3.99999254,    3.99995,    0.00005,    0.00003
     8,    4.00000,       4.00003446,    4.00000,    0.00000,    0.00007
     9,    4.00000,       4.00000000,    4.00000,    0.00000,    0.00000
    10,    4.00000,       4.00000000,    4.00000,    0.00000,    0.00000
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- spammpack-C/LinearAlgebraLibrary/prototype/error_control/SpAMM.f90
+++ spammpack-C/LinearAlgebraLibrary/prototype/error_control/SpAMM.f90
@@ -72,6 +72,10 @@ MODULE SpAMM_TYPES
      MODULE PROCEDURE Trace_qutree
   END INTERFACE
 
+  INTERFACE Copy
+     MODULE PROCEDURE Copy_qutree_eq_qutree
+  END INTERFACE
+
   INTERFACE Add
      MODULE PROCEDURE Add_qutree_pls_qutree
   END INTERFACE
@@ -87,12 +91,9 @@ CONTAINS
     TYPE(QuTree),POINTER        :: qA
     !
     SpAMM_quadcount=0
-    CALL NewQuNode(qA,SpAMM_quadcount)
     !
-    qA%Lev=1
-    qA%Num=SpAMM_quadcount
-    qA%Box(:,1)=(/1,1/)
-    qA%Box(:,2)=(/SpAMM_tiles,SpAMM_tiles/)*BLOCK_SIZE
+
+    CALL NewQuNode(qA,init=.TRUE.)
     CALL SpAMM_Mat2Quad(A,qA)
     qA%Norm=SQRT(qA%Norm)
   END FUNCTION Dense2Quad
@@ -171,6 +172,7 @@ CONTAINS
   FUNCTION Quad2Dense(qA) RESULT(A)
     REAL(DOUBLE),DIMENSION(SpAMM_tiles*BLOCK_SIZE,SpAMM_tiles*BLOCK_SIZE) :: A
     TYPE(QuTree),POINTER :: qA
+    A=Zero
     CALL SpAMM_Quad2Mat(A,qA)
   END FUNCTION Quad2Dense
 
@@ -200,8 +202,8 @@ CONTAINS
   SUBROUTINE DeleteQuad(qA)
     TYPE(QuTree),POINTER :: qA
     CALL SpAMM_DeleteQuad(qA)
-    DEALLOCATE(qA)
-    NULLIFY(qA)
+!    DEALLOCATE(qA)
+!    NULLIFY(qA)
   END SUBROUTINE DeleteQuad
 
   RECURSIVE SUBROUTINE SpAMM_DeleteQuad(qA)
@@ -238,16 +240,28 @@ CONTAINS
     ENDIF
   END SUBROUTINE SpAMM_DeleteQuad
 
-  SUBROUTINE NewQuNode(qA,count)
+  SUBROUTINE NewQuNode(qA,count,init)
+    LOGICAL,OPTIONAL :: init
     INTEGER,OPTIONAL :: count
     TYPE(QuTree), POINTER :: qA
-    ALLOCATE(qA);
-    IF(PRESENT(count))THEN
-       count=count+1
-       qA%Num=count
+    IF(PRESENT(init))THEN
+       ALLOCATE(qA)
+       qA%Num=1
+       qA%Lev=1
+       qA%Siz=SpAMM_tiles*BLOCK_SIZE
+       qA%Box(:,1)=(/1,1/)
+       qA%Box(:,2)=(/SpAMM_tiles,SpAMM_tiles/)*BLOCK_SIZE
     ELSE
-       SpAMM_quadcount=SpAMM_quadcount+1
-       qA%Num=SpAMM_quadcount
+       IF(.NOT.ASSOCIATED(qA))THEN
+          ALLOCATE(qA)
+       ENDIF
+       IF(PRESENT(count))THEN
+          count=count+1
+          qA%Num=count
+       ELSE
+          SpAMM_quadcount=SpAMM_quadcount+1
+          qA%Num=SpAMM_quadcount
+       ENDIF
     ENDIF
     qA%Norm=Zero
     NULLIFY(qA%Quad00)
@@ -293,26 +307,27 @@ CONTAINS
     TYPE(QuTree), POINTER  :: qA
     REAL(DOUBLE) :: trc
     INTEGER :: I
-    WRITE(*,*)ASSOCIATED(qA%Quad00),ASSOCIATED(qA%Quad11)
 
-    IF(.NOT.(ASSOCIATED(qA%Quad00).AND.ASSOCIATED(qA%Quad11)))THEN
-       Trc=Zero
-       RETURN
-    ELSEIF(qA%Siz==BLOCK_SIZE)THEN
-       IF(.NOT.ALLOCATED(qA%Blok))THEN
-          WRITE(*,*)'ASSOCIATED ',ASSOCIATED(qA)
-          WRITE(*,*)' QA NORM = ',qA%Norm
-          STOP 'hosed'
-       ENDIF
+!    WRITE(*,*)'TRACE , Lev =',qA%Lev,' Num = ',qA%Num,ASSOCIATED(qA%Quad00),ASSOCIATED(qA%Quad11)
+
+    IF(qA%Siz==BLOCK_SIZE)THEN
        trc=Zero
+       IF(.NOT.ALLOCATED(qA%Blok))RETURN
        DO I=1,BLOCK_SIZE
           trc=trc+qA%Blok(I,I)
        ENDDO
+    ELSEIF(.NOT.(ASSOCIATED(qA%Quad00).AND.ASSOCIATED(qA%Quad11)))THEN
+!       WRITE(*,*)' Z '
+       Trc=Zero
     ELSEIF(.NOT.ASSOCIATED(qA%Quad11))THEN
+!       WRITE(*,*)' 00 ',ASSOCIATED(qA%Quad00)
+!       WRITE(*,*)' 00 ',ASSOCIATED(qA%Quad00),ASSOCIATED(qA%Quad11)
        trc=Trace_qutree(qA%Quad00)
     ELSEIF(.NOT.ASSOCIATED(qA%Quad00))THEN
+!       WRITE(*,*)' 11 '
        trc=Trace_qutree(qA%Quad11)
     ELSE
+!       WRITE(*,*)' 00+11 '
        trc=Trace_qutree(qA%Quad00)+Trace_qutree(qA%Quad11)
     ENDIF
   END FUNCTION Trace_qutree
@@ -332,18 +347,53 @@ CONTAINS
        CALL Filter_qutree(qA%Quad11,tau)
     ENDIF
   END SUBROUTINE Filter_qutree
+
   !=================================================================
   ! QuTree add routines: C=A+B
   !=================================================================
-  RECURSIVE SUBROUTINE Add_qutree_pls_qutree(qA,qB,qC)
+  SUBROUTINE Add_qutree_pls_qutree(qA,qB,qC)
+    IMPLICIT NONE
+    TYPE(QuTree), POINTER       :: qA,qB,qC
+    INTEGER :: add_quadcount
+
+    IF(.NOT.ASSOCIATED(qC))THEN
+       CALL NewQuNode(qC,init=.TRUE.)
+    ELSE
+       CALL DeleteQuad(qC)
+       CALL NewQuNode(qC,init=.TRUE.)
+    ENDIF
+    add_quadcount=1
+    CALL SpAMM_Add(qA,qB,qC,count=add_quadcount)
+    qC%Norm=SQRT(qC%Norm)
+  END SUBROUTINE Add_qutree_pls_qutree
+
+  SUBROUTINE NodeMssg(Mssg,Node,count)
+    CHARACTER(LEN=*) :: Mssg
+    INTEGER :: count
+    TYPE(QuTree), POINTER       :: Node
+
+!    WRITE(*,11)TRIM(Mssg),Node%Lev,Node%Num,Node%Siz,count, ASSOCIATED(node), &
+!     ASSOCIATED(node%Quad00),ASSOCIATED(node%Quad01),ASSOCIATED(node%Quad10),ASSOCIATED(node%Quad11)
+
+11  FORMAT(A20,' Lev = ',I2,', Num = ',I3,', Siz = ',I4,', Cnt = ',I4,' Aloc = ',5(L2))
+  END SUBROUTINE NodeMssg
+
+
+  RECURSIVE SUBROUTINE SpAMM_Add(qA,qB,qC,count)
     IMPLICIT NONE
     TYPE(QuTree), POINTER    :: qA,qB,qC
+    INTEGER,OPTIONAL :: count
     !
-    IF(.NOT.ASSOCIATED(qA))THEN
-       CALL Copy_qutree_eq_qutree(qB,qC)
-    ELSEIF(.NOT.ASSOCIATED(qB))THEN
-       CALL Copy_qutree_eq_qutree(qA,qC)
-    ELSE
+
+    CALL NodeMssg(' SpAMM_Add init qC ',qC,count)
+    IF(ASSOCIATED(qA).AND.ASSOCIATED(qB))THEN
+       IF(.NOT.ASSOCIATED(qC))THEN
+          CALL NewQuNode(qC,count=count)
+          CALL NodeMssg(' SpAMM_Add newq qC ',qC,count)
+       ELSE
+
+       ENDIF
+       !
        IF(qA%Siz==BLOCK_SIZE)THEN
           IF(.NOT.ALLOCATED(qC%Blok))THEN
              ALLOCATE(qC%Blok(1:BLOCK_SIZE,1:BLOCK_SIZE))
@@ -357,53 +407,145 @@ CONTAINS
           ! Bounds
           qC%Norm=SUM(qC%Blok(1:BLOCK_SIZE,1:BLOCK_SIZE)**2)
        ELSE
-          CALL NewQuNode(qC%Quad00)
-          CALL NewQuNode(qC%Quad01)
-          CALL NewQuNode(qC%Quad10)
-          CALL NewQuNode(qC%Quad11)
-          CALL Add_qutree_pls_qutree(qA%Quad00,qB%Quad00,qC%Quad00)
-          CALL Add_qutree_pls_qutree(qA%Quad01,qB%Quad01,qC%Quad01)
-          CALL Add_qutree_pls_qutree(qA%Quad10,qB%Quad10,qC%Quad10)
-          CALL Add_qutree_pls_qutree(qA%Quad11,qB%Quad11,qC%Quad11)
-          qC%Norm=qC%Quad00%Norm+qC%Quad01%Norm+qC%Quad10%Norm+qC%Quad11%Norm
-          qC%Quad00%Norm=SQRT(qC%Quad00%Norm)
-          qC%Quad10%Norm=SQRT(qC%Quad10%Norm)
-          qC%Quad01%Norm=SQRT(qC%Quad01%Norm)
-          qC%Quad11%Norm=SQRT(qC%Quad11%Norm)
-          IF(qC%Lev==1)THEN
-             qC%Norm=SQRT(qC%Norm)
+          qC%Norm=Zero
+
+          IF(ASSOCIATED(qA%Quad00).OR.ASSOCIATED(qB%Quad00))THEN
+             IF(.NOT.ASSOCIATED(qC%Quad00))THEN
+                CALL NewQuNode(qC%Quad00,count=count)
+             ELSE
+                count=count+1
+                qC%Quad00%Num=count
+             ENDIF
+             qC%Quad00%Lev=qA%Quad00%Lev
+             qC%Quad00%Siz=qA%Quad00%Siz
+             qC%Quad00%Box=qA%Quad00%Box
+             CALL SpAMM_Add(qA%Quad00,qB%Quad00,qC%Quad00,count)
+             qC%Norm=qC%Norm+qC%Quad00%Norm
+             qC%Quad00%Norm=SQRT(qC%Quad00%Norm)
+          ELSEIF(ASSOCIATED(qC%Quad00))THEN
+             CALL DeleteQuad(qC%Quad00)
+          ENDIF
+          IF(ASSOCIATED(qA%Quad01).OR.ASSOCIATED(qB%Quad01))THEN
+             IF(.NOT.ASSOCIATED(qC%Quad01))THEN
+                CALL NewQuNode(qC%Quad01,count=count)
+             ELSE
+                count=count+1
+                qC%Quad01%Num=count
+             ENDIF
+             qC%Quad01%Lev=qA%Quad01%Lev
+             qC%Quad01%Siz=qA%Quad01%Siz
+             qC%Quad01%Box=qA%Quad01%Box
+             CALL SpAMM_Add(qA%Quad01,qB%Quad01,qC%Quad01,count)
+             qC%Norm=qC%Norm+qC%Quad01%Norm
+             qC%Quad01%Norm=SQRT(qC%Quad01%Norm)
+          ELSEIF(ASSOCIATED(qC%Quad01))THEN
+             CALL DeleteQuad(qC%Quad01)
+          ENDIF
+          IF(ASSOCIATED(qA%Quad10).OR.ASSOCIATED(qB%Quad10))THEN
+             IF(.NOT.ASSOCIATED(qC%Quad10))THEN
+                CALL NewQuNode(qC%Quad10,count=count)
+             ELSE
+                count=count+1
+                qC%Quad10%Num=count
+             ENDIF
+             qC%Quad10%Lev=qA%Quad10%Lev
+             qC%Quad10%Siz=qA%Quad10%Siz
+             qC%Quad10%Box=qA%Quad10%Box
+             CALL SpAMM_Add(qA%Quad10,qB%Quad10,qC%Quad10,count)
+             qC%Norm=qC%Norm+qC%Quad10%Norm
+             qC%Quad10%Norm=SQRT(qC%Quad10%Norm)
+          ELSEIF(ASSOCIATED(qC%Quad10))THEN
+             CALL DeleteQuad(qC%Quad10)
+          ENDIF
+          IF(ASSOCIATED(qA%Quad11).OR.ASSOCIATED(qB%Quad11))THEN
+             IF(.NOT.ASSOCIATED(qC%Quad11))THEN
+                CALL NewQuNode(qC%Quad11,count=count)
+             ELSE
+                count=count+1
+                qC%Quad11%Num=count
+             ENDIF
+             qC%Quad11%Lev=qA%Quad11%Lev
+             qC%Quad11%Siz=qA%Quad11%Siz
+             qC%Quad11%Box=qA%Quad11%Box
+             CALL SpAMM_Add(qA%Quad11,qB%Quad11,qC%Quad11,count)
+             qC%Norm=qC%Norm+qC%Quad11%Norm
+             qC%Quad11%Norm=SQRT(qC%Quad11%Norm)
+          ELSEIF(ASSOCIATED(qC%Quad11))THEN
+             CALL DeleteQuad(qC%Quad11)
           ENDIF
        ENDIF
+    ELSEIF(.NOT.ASSOCIATED(qA))THEN
+       CALL SpAMM_Copy(qB,qC,count=count)
+    ELSEIF(.NOT.ASSOCIATED(qB))THEN
+       CALL SpAMM_Copy(qA,qC,count=count)
     ENDIF
-  END SUBROUTINE Add_qutree_pls_qutree
+  END SUBROUTINE SpAMM_Add
   !=================================================================
   ! QuTree copy routines: C=A
   !=================================================================
-  RECURSIVE SUBROUTINE Copy_qutree_eq_qutree(qA,qC)
+  SUBROUTINE Copy_qutree_eq_qutree(qA,qC)
+    IMPLICIT NONE
+    TYPE(QuTree), POINTER       :: qA,qC
+    INTEGER :: node_count
+    IF(.NOT.ASSOCIATED(qC)) &
+    CALL NewQuNode(qC,SpAMM_quadcount)
+    qC%Num=1
+    qC%Lev=1
+    node_count=0
+    qC%Box(:,1)=(/1,1/)
+    qC%Box(:,2)=(/SpAMM_tiles,SpAMM_tiles/)*BLOCK_SIZE
+    CALL SpAMM_Copy(qA,qC,count=node_count)
+  END SUBROUTINE Copy_qutree_eq_qutree
+
+  RECURSIVE SUBROUTINE SpAMM_Copy(qA,qC,count)
     IMPLICIT NONE
     TYPE(QuTree), POINTER    :: qA,qC
+    INTEGER, OPTIONAL :: count
     !
     IF(.NOT.ASSOCIATED(qA))RETURN
-    IF(.NOT.ASSOCIATED(qC))ALLOCATE(qC)
+    !
+    IF(.NOT.ASSOCIATED(qC))THEN
+       CALL NewQuNode(qC,count=count)
+    ELSEIF(PRESENT(count))THEN
+       count=count+1
+       qC%Num=count
+    ELSE
+       SpAMM_quadcount=SpAMM_quadcount+1
+       qC%Num=SpAMM_quadcount
+    ENDIF
+    qC%Siz=qA%Siz
+    qC%Lev=qA%Lev
+    qC%Box=qA%Box
+    qC%Norm=qA%Norm
+    CALL NodeMssg(' SpAMM_Copy qC ',qC,count)
     IF(qA%Siz==BLOCK_SIZE)THEN
-       IF(.NOT.ALLOCATED(qC%Blok))THEN
+       IF(.NOT.ALLOCATED(qC%Blok)) &
           ALLOCATE(qC%Blok(1:BLOCK_SIZE,1:BLOCK_SIZE))
-          qC%Norm=qA%Norm
-          qC%Lev=qA%Lev
-          qC%Siz=BLOCK_SIZE
-          qC%Blok=qA%Blok
-       ELSE
-          ALLOCATE(qC%Quad00)
-          ALLOCATE(qC%Quad01)
-          ALLOCATE(qC%Quad10)
-          ALLOCATE(qC%Quad11)
-          CALL Copy_qutree_eq_qutree(qA%Quad00,qC%Quad00)
-          CALL Copy_qutree_eq_qutree(qA%Quad01,qC%Quad01)
-          CALL Copy_qutree_eq_qutree(qA%Quad10,qC%Quad10)
-          CALL Copy_qutree_eq_qutree(qA%Quad11,qC%Quad11)
+       qC%Blok=qA%Blok
+    ELSE
+       IF(ASSOCIATED(qA%Quad00))THEN
+          CALL SpAMM_Copy(qA%Quad00,qC%Quad00,count=count)
+       ELSEIF(ASSOCIATED(qC%Quad00))THEN
+          CALL DeleteQuad(qC%Quad00)
+       ENDIF
+       IF(ASSOCIATED(qA%Quad01))THEN
+          CALL SpAMM_Copy(qA%Quad01,qC%Quad01,count=count)
+       ELSEIF(ASSOCIATED(qC%Quad01))THEN
+          CALL DeleteQuad(qC%Quad01)
+       ENDIF
+       IF(ASSOCIATED(qA%Quad10))THEN
+          CALL SpAMM_Copy(qA%Quad10,qC%Quad10,count=count)
+       ELSEIF(ASSOCIATED(qC%Quad10))THEN
+          CALL DeleteQuad(qC%Quad10)
+       ENDIF
+       IF(ASSOCIATED(qA%Quad11))THEN
+          CALL SpAMM_Copy(qA%Quad11,qC%Quad11,count=count)
+       ELSEIF(ASSOCIATED(qC%Quad11))THEN
+          CALL DeleteQuad(qC%Quad11)
        ENDIF
     ENDIF
-  END SUBROUTINE Copy_qutree_eq_qutree
+  END SUBROUTINE SpAMM_Copy
+
   !=================================================================
   ! QuTree multiply routines
   !=================================================================
@@ -433,17 +575,23 @@ CONTAINS
     TYPE(QuTree), POINTER       :: qA,qB,qC
     REAL(DOUBLE),OPTIONAL       :: tolerance
     REAL(DOUBLE)                :: Saved_tolerance
+
+    CALL NodeMssg('SpAMM_mult 1 A',qA,1)
+    CALL NodeMssg('SpAMM_mult 1 B',qB,1)
+
     IF(PRESENT(tolerance))THEN
        Saved_tolerance=SpAMM_tolerance
        SpAMM_tolerance=tolerance
     ENDIF
-    SpAMM_quadcount=0
-    CALL NewQuNode(qC,SpAMM_quadcount)
-    qC%Num=1
-    qC%Lev=1
-    SpAMM_multiplies=Zero
-    qC%Box(:,1)=(/1,1/)
-    qC%Box(:,2)=(/SpAMM_tiles,SpAMM_tiles/)*BLOCK_SIZE
+
+
+
+    IF(ASSOCIATED(qC))THEN
+       CALL DeleteQuad(qC)
+    ENDIF
+
+    CALL NewQuNode(qC,init=.TRUE.)
+    SpAMM_quadcount=1
     CALL SpAMM_Multiply(qC,qA,qB)
     IF(PRESENT(tolerance))THEN
        SpAMM_tolerance=Saved_tolerance
@@ -456,7 +604,6 @@ CONTAINS
     REAL(DOUBLE), DIMENSION(1:BLOCK_SIZE,1:BLOCK_SIZE) :: CTmp
     LOGICAL :: do00x00,do01x10,do10x00,do11x10, &
          do00x01,do01x11,do10x01,do11x11
-    !
     ! Bounds
     qC%Siz=qA%Siz
     qC%Box(1,1)=qA%Box(2,1)
@@ -491,6 +638,8 @@ CONTAINS
        do11x10=ASSOCIATED(qA%Quad11).AND.ASSOCIATED(qB%Quad10)
        do10x01=ASSOCIATED(qA%Quad10).AND.ASSOCIATED(qB%Quad01)
        do11x11=ASSOCIATED(qA%Quad11).AND.ASSOCIATED(qB%Quad11)
+!       WRITE(*,11)do00x00,do01x10,do00x01,do01x11,do10x00,do11x10,do10x01,do11x11
+!11     FORMAT(8(L2))
        ! SpAMM, SpAMM and SpAMM
        IF(do00x00)do00x00=do00x00.AND.(qA%Quad00%Norm*qB%Quad00%Norm>SpAMM_tolerance)
        IF(do01x10)do01x10=do01x10.AND.(qA%Quad01%Norm*qB%Quad10%Norm>SpAMM_tolerance)
@@ -502,7 +651,7 @@ CONTAINS
        IF(do11x11)do11x11=do11x11.AND.(qA%Quad11%Norm*qB%Quad11%Norm>SpAMM_tolerance)
        ! 00=00*00+01*10
        IF(do00x00.OR.do01x10)THEN
-          IF(.NOT.ASSOCIATED(qC%Quad00))CALL NewQuNode(qC%Quad00,SpAMM_quadcount)
+          IF(.NOT.ASSOCIATED(qC%Quad00))CALL NewQuNode(qC%Quad00,count=SpAMM_quadcount)
           IF(do00x00)CALL SpAMM_Multiply(qC%Quad00,qA%Quad00,qB%Quad00)
           IF(do01x10)CALL SpAMM_Multiply(qC%Quad00,qA%Quad01,qB%Quad10)
           qC%Norm=qC%Norm+qC%Quad00%Norm
@@ -512,7 +661,7 @@ CONTAINS
        ENDIF
        ! 01=00*01+01*11
        IF(do00x01.OR.do01x11)THEN
-          IF(.NOT.ASSOCIATED(qC%Quad01))CALL NewQuNode(qC%Quad01,SpAMM_quadcount)
+          IF(.NOT.ASSOCIATED(qC%Quad01))CALL NewQuNode(qC%Quad01,count=SpAMM_quadcount)
           IF(do00x01)CALL SpAMM_Multiply(qC%Quad01,qA%Quad00,qB%Quad01)
           IF(do01x11)CALL SpAMM_Multiply(qC%Quad01,qA%Quad01,qB%Quad11)
           qC%Norm=qC%Norm+qC%Quad01%Norm
@@ -522,7 +671,7 @@ CONTAINS
        ENDIF
        ! 10=10*00+11*10
        IF(do10x00.OR.do11x10)THEN
-          IF(.NOT.ASSOCIATED(qC%Quad10))CALL NewQuNode(qC%Quad10,SpAMM_quadcount)
+          IF(.NOT.ASSOCIATED(qC%Quad10))CALL NewQuNode(qC%Quad10,count=SpAMM_quadcount)
           IF(do10x00)CALL SpAMM_Multiply(qC%Quad10,qA%Quad10,qB%Quad00)
           IF(do11x10)CALL SpAMM_Multiply(qC%Quad10,qA%Quad11,qB%Quad10)
           qC%Norm=qC%Norm+qC%Quad10%Norm
@@ -532,7 +681,7 @@ CONTAINS
        ENDIF
        ! 11=10*01+11*11
        IF(do10x01.OR.do11x11)THEN
-          IF(.NOT.ASSOCIATED(qC%Quad11))CALL NewQuNode(qC%Quad11,SpAMM_quadcount)
+          IF(.NOT.ASSOCIATED(qC%Quad11))CALL NewQuNode(qC%Quad11,count=SpAMM_quadcount)
           IF(do10x01)CALL SpAMM_Multiply(qC%Quad11,qA%Quad10,qB%Quad01)
           IF(do11x11)CALL SpAMM_Multiply(qC%Quad11,qA%Quad11,qB%Quad11)
           qC%Norm=qC%Norm+qC%Quad11%Norm
@@ -544,41 +693,44 @@ CONTAINS
           qC%Norm=SQRT(qC%Norm)
        ENDIF
     ENDIF
-
   END SUBROUTINE SpAMM_Multiply
 
-     SUBROUTINE TC2(P,P2,Tmp1,Norm,TrP,I)
-       TYPE(QuTree),POINTER :: P,P2,Tmp1
-       REAL(DOUBLE) :: Norm, CR1, CR2, TrP, TrP2
-       INTEGER      :: I
-       !-------------------------------------------------------------------------------
-       IF (I.EQ.1) THEN
-          TrP=Trace(P)
-       ENDIF
-       WRITE(*,*)'1'
-       CALL Multiply(P,P,P2)             ! The only multiplication is a square
-       WRITE(*,*)'2'
-       TrP2=Trace(P2)
-       WRITE(*,*)'3'
-       CR1 = ABS(TrP2-Norm)              ! CR1 = Occupation error criteria
-       CR2 = ABS(2.D0*TrP - TrP2 - Norm) ! CR2 = Occupation error criteria
-       WRITE(*,33)I,NORM,TrP,CR1,CR2
-33     FORMAT(I4,", ",F10.5,", ",F16.8,", ",F10.5,", ",F10.5)
-       IF (CR1 < CR2) THEN               ! Too many states
-          P=>P2
-       ELSE
-          CALL Multiply(P,Two)
-       WRITE(*,*)'4'
-          CALL Multiply(P2,-One)
-       WRITE(*,*)'5'
-          CALL Add(P,P2,Tmp1)             ! P = 2P-P^2
-       WRITE(*,*)'6'
-          P=>Tmp1
-       ENDIF
-       WRITE(*,*)'7'
+  SUBROUTINE TC2(P,P2,Tmp1,Norm,TrP,I)
+    TYPE(QuTree),POINTER :: P,P2,Tmp1 , Tmp2
+    REAL(DOUBLE) :: Norm, CR1, CR2, TrP, TrP2,    TrPd,TrPd2
+    INTEGER      :: I
+
+    REAL(DOUBLE),DIMENSION(4,4) :: dP,dP2,dTmp1, dTmp2
+    !-------------------------------------------------------------------------------
+    IF (I.EQ.1) THEN
        TrP=Trace(P)
-       WRITE(*,*)'8'
-     END SUBROUTINE TC2
+    ENDIF
+
+    CALL Multiply(P,P,P2)             ! The only multiplication is a square
+    TrP2=Trace(P2)
+    CR1 = ABS(TrP2-Norm)              ! CR1 = Occupation error criteria
+    CR2 = ABS(2.D0*TrP - TrP2 - Norm) ! CR2 = Occupation error criteria
+
+   WRITE(*,33)I,NORM,TrP,TrP2,CR1,CR2
+33 FORMAT(I4,", ",F10.5,", ",F16.8,", ",F10.5,", ",F10.5,", ",F10.5)
+
+   IF (CR1 < CR2) THEN               ! Too many states
+      CALL Copy(P2,P)                !       P=>P2
+    ELSE
+       CALL Multiply(P,Two)
+       CALL Multiply(P2,-One)
+       CALL Add(P,P2,Tmp1)             ! P = 2P-P^2
+       CALL Copy(Tmp1,P)
+    ENDIF
+    TrP=Trace(P)
+
+!  CR =    2.000    2.00000000000   1.6730701243227033       0.32692987567729670       0.32692987567729759
+!  CR =    2.000    1.67307012432   1.3728610345479997       0.62713896545200032       2.67207859025930805E-002
+!  CR =    2.000    1.97327921409   1.8845595332763059       0.11544046672369412       6.19988949185081850E-002
+!  CR =    2.000    2.06199889491   1.9985696681490941       1.43033185090590820E-003  0.12542812168792228
+
+
+  END SUBROUTINE TC2
 
    END MODULE SpAMM_TYPES
 
@@ -620,7 +772,6 @@ PROGRAM SpAMM_TEST
   A=Zero
   A(1:N_OLD,1:N_OLD)=A_NOPADDING(1:N_OLD,1:N_OLD)
   qF=>Dense2Quad(A)
-  CALL Print_Quad(qF)
   !--------------------------------------------------
   OPEN(UNIT=66,FILE=TRIM(NAME)//".p_guess_ortho")
   DO I=1,N_OLD
@@ -632,30 +783,14 @@ PROGRAM SpAMM_TEST
   A=Zero
   A(1:N_OLD,1:N_OLD)=A_NOPADDING(1:N_OLD,1:N_OLD)
   qP=>Dense2Quad(A)
-  CALL Print_Quad(qP)
   !--------------------------------------------------
 !  DEALLOCATE(A)
   DEALLOCATE(A_NOPADDING)
   !
-  ALLOCATE(qTmp1)
-  ALLOCATE(qTmp2)
-!!$
-!!$
-!!$  ALLOCATE(C(1:N,1:N))
-!!$  ALLOCATE(C2(1:N,1:N))
-!!$
-!!$  CALL Multiply(qP,qP,1D-30)
-!!$  C=Quad2Dense(qC)
-!!$  !
-!!$  C2=MATMUL(A,A)
-!!$
-!!$  DO I=1,4
-!!$     DO J=1,4
-!!$        WRITE(*,*)C(I,J),C2(I,J)
-!!$     ENDDO
-!!$  ENDDO
-!!$
-!!$!  STOP
+  NULLIFY(qTmp1)
+  NULLIFY(qTmp2)
+  CALL NewQuNode(qTmp1,init=.TRUE.)
+  CALL NewQuNode(qTmp2,init=.TRUE.)
   !
   Occ0 = 0.D0
   Occ1 = 0.D0
