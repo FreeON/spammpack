cd5eb76079069f991787f0619a35628f776ac897
Nicolas Bock <nicolasbock@gmail.com>
2010-09-23 15:12:54 -0600

    Added new stream_kernel.
    stream_kernel_20:
      Simply moving the store operation on the C block outside the norm product
      logic causes the performance to drop significantly. I guess the compiler is
      using poor register locality because the store is outside the if() scope.
      Using precomputed norm products also reduces performance. The generate
      script generates these 2 cases.

--- spammpack-C/LinearAlgebraLibrary/prototype/stream_multiply/generate_SSE_intrinsics.py
+++ spammpack-C/LinearAlgebraLibrary/prototype/stream_multiply/generate_SSE_intrinsics.py
@@ -22,6 +22,16 @@ parser.add_option("--name",
     type = "string",
     default = "stream_kernel")
 
+parser.add_option("--use-precomputed-norm-products",
+    action = "store_true",
+    help = "use precomputed norm products",
+    dest = "precomputed_norm_products")
+
+parser.add_option("--store-outside-if",
+    action = "store_true",
+    help = "move the C block store outside the if() block",
+    dest = "store_outside_if")
+
 ( options, arguments ) = parser.parse_args()
 
 # Check N.
@@ -80,6 +90,9 @@ print
 print "  __m128 A_element;"
 print "  __m128 B_row;"
 print "  __m128 C_row[4];"
+if options.precomputed_norm_products:
+  print
+  print "  char norm_product[%d][%d];" % (options.N**2, options.N**2)
 print
 print "  /* Divide number of stream elements by %d to simulate stride of %d. */" % (options.N**3, options.N**3)
 print "  max_stream_index = number_stream_elements/%d;" % (options.N**3)
@@ -94,31 +107,50 @@ print "    B = multiply_stream[stream_index].B_block;"
 print "    C = multiply_stream[stream_index].C_block;"
 print "    norm = multiply_stream[stream_index].norm;"
 
+if options.precomputed_norm_products:
+  print
+  print "    /* Calculate norms. */"
+  for i in range(options.N):
+    for j in range(options.N):
+      for k in range(options.N):
+        norm_index_A = i*options.N+k
+        norm_index_B = k*options.N+j+options.N**2
+        print "    norm_product[%d][%d] = (norm[%d]*norm[%d] >= tolerance);" % (i*options.N+k, k*options.N+j, norm_index_A, norm_index_B)
+
 for i in range(options.N):
   for j in range(options.N):
+    print
+    print "    /* Reset C(%d,%d) matrix accumulators */" % (i+1, j+1)
+    print "    C_row[0] = _mm_setzero_ps();"
+    print "    C_row[1] = _mm_setzero_ps();"
+    print "    C_row[2] = _mm_setzero_ps();"
+    print "    C_row[3] = _mm_setzero_ps();"
+
     print
     sys.stdout.write("    if (")
     for k in range(options.N):
       norm_index_A = i*options.N+k
       norm_index_B = k*options.N+j+options.N**2
-      sys.stdout.write("norm[%d]*norm[%d] >= tolerance" % (norm_index_A, norm_index_B))
+      if options.precomputed_norm_products:
+        sys.stdout.write("norm_product[%d][%d]" % (i*options.N+k, k*options.N+j))
+      else:
+        sys.stdout.write("norm[%d]*norm[%d] >= tolerance" % (norm_index_A, norm_index_B))
       if k < options.N-1:
         sys.stdout.write(" &&\n")
         sys.stdout.write("        ")
       else:
         sys.stdout.write(")\n")
-
     print "    {"
+
     for k in range(options.N):
+      #print "    if (norm_product[%d][%d])" % (i*options.N+k, k*options.N+j)
+      #print "    {"
       print "      /* A(%d,%d)*B(%d,%d) = C(%d,%d). */" % (i+1, k+1, k+1, j+1, i+1, j+1)
       print "      for (i = 0; i < 4; i++)"
       print "      {"
       print "        A_element = _mm_load_ps(&A[(i*4+0)*4+A_OFFSET_%d%d]);" % (i+1, k+1)
       print "        B_row = _mm_load_ps(&B[0*4+B_OFFSET_%d%d]);" % (k+1, j+1)
-      if k == 0:
-        print "        C_row[i] = _mm_mul_ps(A_element, B_row);"
-      else:
-        print "        C_row[i] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[i]);"
+      print "        C_row[i] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[i]);"
       print
       print "        A_element = _mm_load_ps(&A[(i*4+1)*4+A_OFFSET_%d%d]);" % (i+1, k+1)
       print "        B_row = _mm_load_ps(&B[1*4+B_OFFSET_%d%d]);" % (k+1, j+1)
@@ -132,16 +164,30 @@ for i in range(options.N):
       print "        B_row = _mm_load_ps(&B[3*4+B_OFFSET_%d%d]);" % (k+1, j+1)
       print "        C_row[i] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[i]);"
       print "      }"
-      print
+      #print "    }"
+      if k < options.N-1:
+        print
 
-    print "      /* Store C block. */"
-    print "      for (i = 0; i < 4; i++)"
-    print "      {"
-    print "        C_row[i] = _mm_mul_ps(alpha_row, C_row[i]);"
-    print "        C_row[i] = _mm_add_ps(_mm_load_ps(&C[i*4+C_OFFSET_%d%d]), C_row[i]);" % (i+1, j+1)
-    print "        _mm_store_ps(&C[i*4+C_OFFSET_%d%d], C_row[i]);" % (i+1, j+1)
-    print "      }"
-    print "    }"
+    if options.store_outside_if:
+      print "    }"
+      print
+      print "    /* Store C(%d,%d) block. */" % (i+1, j+1)
+      print "    for (i = 0; i < 4; i++)"
+      print "    {"
+      print "      C_row[i] = _mm_mul_ps(alpha_row, C_row[i]);"
+      print "      C_row[i] = _mm_add_ps(_mm_load_ps(&C[i*4+C_OFFSET_%d%d]), C_row[i]);" % (i+1, j+1)
+      print "      _mm_store_ps(&C[i*4+C_OFFSET_%d%d], C_row[i]);" % (i+1, j+1)
+      print "    }"
+    else:
+      print
+      print "      /* Store C(%d,%d) block. */" % (i+1, j+1)
+      print "      for (i = 0; i < 4; i++)"
+      print "      {"
+      print "        C_row[i] = _mm_mul_ps(alpha_row, C_row[i]);"
+      print "        C_row[i] = _mm_add_ps(_mm_load_ps(&C[i*4+C_OFFSET_%d%d]), C_row[i]);" % (i+1, j+1)
+      print "        _mm_store_ps(&C[i*4+C_OFFSET_%d%d], C_row[i]);" % (i+1, j+1)
+      print "      }"
+      print "    }"
 
 print "  }"
 print "}"
