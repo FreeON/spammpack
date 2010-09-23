2c856bddc826de2a70e81fd67c1ba329dfd55230
Nicolas Bock <nicolasbock@gmail.com>
2010-09-23 13:28:42 -0600

    Reverting back to previous version of generate script.

--- spammpack-C/LinearAlgebraLibrary/prototype/stream_multiply/generate_SSE_intrinsics.py
+++ spammpack-C/LinearAlgebraLibrary/prototype/stream_multiply/generate_SSE_intrinsics.py
@@ -34,6 +34,22 @@ if 2**d != options.N:
 print "#include <stdlib.h>"
 print "#include <xmmintrin.h>"
 
+# Generate offsets.
+print
+for i in range(options.N):
+  for j in range(options.N):
+    print "#define A_OFFSET_%d%d (%d*%d+%d)*64 // %d" % (i+1, j+1, i, options.N, j, (i*options.N+j)*64)
+
+print
+for i in range(options.N):
+  for j in range(options.N):
+    print "#define B_OFFSET_%d%d (%d*%d+%d)*16 // %d" % (i+1, j+1, i, options.N, j, (i*options.N+j)*16)
+
+print
+for i in range(options.N):
+  for j in range(options.N):
+    print "#define C_OFFSET_%d%d (%d*%d+%d)*16 // %d" % (i+1, j+1, i, options.N, j, (i*options.N+j)*16)
+
 print
 print "struct multiply_stream_t"
 print "{"
@@ -49,58 +65,7 @@ print "    float alpha,"
 print "    float tolerance,"
 print "    struct multiply_stream_t *multiply_stream)"
 print "{"
-
-# Generate offsets.
-print
-sys.stdout.write("  static const int A_OFFSET[%d][%d] =\n" % (options.N, options.N))
-sys.stdout.write("  {\n")
-for i in range(options.N):
-  sys.stdout.write("    {\n")
-  for j in range(options.N):
-    sys.stdout.write("      %d" % ((i*options.N+j)*64))
-    if j < options.N-1:
-      sys.stdout.write(",")
-    sys.stdout.write(" /* (%d*%d+%d)*64 */\n" % (i, options.N, j))
-  sys.stdout.write("    }")
-  if i < options.N-1:
-    sys.stdout.write(",")
-  sys.stdout.write("\n")
-sys.stdout.write("  };\n")
-
-print
-sys.stdout.write("  static const int B_OFFSET[%d][%d] =\n" % (options.N, options.N))
-sys.stdout.write("  {\n")
-for i in range(options.N):
-  sys.stdout.write("    {\n")
-  for j in range(options.N):
-    sys.stdout.write("      %d" % ((i*options.N+j)*16))
-    if j < options.N-1:
-      sys.stdout.write(",")
-    sys.stdout.write(" /* (%d*%d+%d)*16 */\n" % (i, options.N, j))
-  sys.stdout.write("    }")
-  if i < options.N-1:
-    sys.stdout.write(",")
-  sys.stdout.write("\n")
-sys.stdout.write("  };\n")
-
-print
-sys.stdout.write("  static const int C_OFFSET[%d][%d] =\n" % (options.N, options.N))
-sys.stdout.write("  {\n")
-for i in range(options.N):
-  sys.stdout.write("    {\n")
-  for j in range(options.N):
-    sys.stdout.write("      %d" % ((i*options.N+j)*16))
-    if j < options.N-1:
-      sys.stdout.write(",")
-    sys.stdout.write(" /* (%d*%d+%d)*16 */\n" % (i, options.N, j))
-  sys.stdout.write("    }")
-  if i < options.N-1:
-    sys.stdout.write(",")
-  sys.stdout.write("\n")
-sys.stdout.write("  };\n")
-
-print
-print "  short int i, j, k, l;"
+print "  short int i;"
 print "  unsigned int stream_index;"
 print "  unsigned int max_stream_index;"
 print
@@ -129,49 +94,54 @@ print "    B = multiply_stream[stream_index].B_block;"
 print "    C = multiply_stream[stream_index].C_block;"
 print "    norm = multiply_stream[stream_index].norm;"
 
-print
-print "    for (i = 0; i < %d; i++) {" % (options.N)
-print "      for (j = 0; j < %d; j++)" % (options.N)
-print "      {"
-print "        C_row[0] = _mm_setzero_ps();"
-print "        C_row[1] = _mm_setzero_ps();"
-print "        C_row[2] = _mm_setzero_ps();"
-print "        C_row[3] = _mm_setzero_ps();"
-print
-print "        for (k = 0; k < %d; k++)" % (options.N)
-print "        {"
-print "          if (norm[i*%d+k]*norm[k*%d+j+%d] < tolerance) { continue; }" % (options.N, options.N, options.N**2)
-print "          else"
-print "          {"
-print "            for (l = 0; l < 4; l++)"
-print "            {"
-print "              A_element = _mm_load_ps(&A[(l*4+0)*4+A_OFFSET[i][k]]);"
-print "              B_row = _mm_load_ps(&B[0*4+B_OFFSET[k][j]]);"
-print "              C_row[l] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[l]);"
-print
-print "              A_element = _mm_load_ps(&A[(l*4+1)*4+A_OFFSET[i][k]]);"
-print "              B_row = _mm_load_ps(&B[1*4+B_OFFSET[k][j]]);"
-print "              C_row[l] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[l]);"
-print
-print "              A_element = _mm_load_ps(&A[(l*4+2)*4+A_OFFSET[i][k]]);"
-print "              B_row = _mm_load_ps(&B[2*4+B_OFFSET[k][j]]);"
-print "              C_row[l] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[l]);"
-print
-print "              A_element = _mm_load_ps(&A[(l*4+3)*4+A_OFFSET[i][k]]);"
-print "              B_row = _mm_load_ps(&B[3*4+B_OFFSET[k][j]]);"
-print "              C_row[l] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[l]);"
-print "            }"
-print "          }"
-print "        }"
-print
-print "        /* Store C block. */"
-print "        for (l = 0; l < 4; l++)"
-print "        {"
-print "          C_row[l] = _mm_mul_ps(alpha_row, C_row[l]);"
-print "          C_row[l] = _mm_add_ps(_mm_load_ps(&C[l*4+C_OFFSET[i][j]]), C_row[l]);"
-print "          _mm_store_ps(&C[l*4+C_OFFSET[i][j]], C_row[l]);"
-print "        }"
-print "      }"
-print "    }"
+for i in range(options.N):
+  for j in range(options.N):
+    print
+    sys.stdout.write("    if (")
+    for k in range(options.N):
+      norm_index_A = i*options.N+k
+      norm_index_B = k*options.N+j+options.N**2
+      sys.stdout.write("norm[%d]*norm[%d] >= tolerance" % (norm_index_A, norm_index_B))
+      if k < options.N-1:
+        sys.stdout.write(" &&\n")
+        sys.stdout.write("        ")
+      else:
+        sys.stdout.write(")\n")
+
+    print "    {"
+    for k in range(options.N):
+      print "      /* A(%d,%d)*B(%d,%d) = C(%d,%d). */" % (i+1, k+1, k+1, j+1, i+1, j+1)
+      print "      for (i = 0; i < 4; i++)"
+      print "      {"
+      print "        A_element = _mm_load_ps(&A[(i*4+0)*4+A_OFFSET_%d%d]);" % (i+1, k+1)
+      print "        B_row = _mm_load_ps(&B[0*4+B_OFFSET_%d%d]);" % (k+1, j+1)
+      if k == 0:
+        print "        C_row[i] = _mm_mul_ps(A_element, B_row);"
+      else:
+        print "        C_row[i] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[i]);"
+      print
+      print "        A_element = _mm_load_ps(&A[(i*4+1)*4+A_OFFSET_%d%d]);" % (i+1, k+1)
+      print "        B_row = _mm_load_ps(&B[1*4+B_OFFSET_%d%d]);" % (k+1, j+1)
+      print "        C_row[i] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[i]);"
+      print
+      print "        A_element = _mm_load_ps(&A[(i*4+2)*4+A_OFFSET_%d%d]);" % (i+1, k+1)
+      print "        B_row = _mm_load_ps(&B[2*4+B_OFFSET_%d%d]);" % (k+1, j+1)
+      print "        C_row[i] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[i]);"
+      print
+      print "        A_element = _mm_load_ps(&A[(i*4+3)*4+A_OFFSET_%d%d]);" % (i+1, k+1)
+      print "        B_row = _mm_load_ps(&B[3*4+B_OFFSET_%d%d]);" % (k+1, j+1)
+      print "        C_row[i] = _mm_add_ps(_mm_mul_ps(A_element, B_row), C_row[i]);"
+      print "      }"
+      print
+
+    print "      /* Store C block. */"
+    print "      for (i = 0; i < 4; i++)"
+    print "      {"
+    print "        C_row[i] = _mm_mul_ps(alpha_row, C_row[i]);"
+    print "        C_row[i] = _mm_add_ps(_mm_load_ps(&C[i*4+C_OFFSET_%d%d]), C_row[i]);" % (i+1, j+1)
+    print "        _mm_store_ps(&C[i*4+C_OFFSET_%d%d], C_row[i]);" % (i+1, j+1)
+    print "      }"
+    print "    }"
+
 print "  }"
 print "}"
