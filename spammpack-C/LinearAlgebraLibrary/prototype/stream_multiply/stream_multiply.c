ae662a84b3fc542ff6880aebeb218be072fa6bd7
Nicolas Bock <nicolasbock@gmail.com>
2010-09-23 15:12:54 -0600

    Added new stream_kernel.
    stream_kernel_20:
      Simply moving the store operation on the C block outside the norm product
      logic causes the performance to drop significantly. I guess the compiler is
      using poor register locality because the store is outside the if() scope.
      Using precomputed norm products also reduces performance. The generate
      script generates these 2 cases.

--- spammpack-C/LinearAlgebraLibrary/prototype/stream_multiply/stream_multiply.c
+++ spammpack-C/LinearAlgebraLibrary/prototype/stream_multiply/stream_multiply.c
@@ -42,7 +42,8 @@
 //#define STREAM_KERNEL_16
 //#define STREAM_KERNEL_17
 //#define STREAM_KERNEL_18
-#define STREAM_KERNEL_19
+//#define STREAM_KERNEL_19
+#define STREAM_KERNEL_20
 //#define POINTER_CHASE
 //#define C_KERNEL
 //#define NAIVE_KERNEL
@@ -106,7 +107,7 @@ struct multiply_stream_t
   char mask[8];
 #elif defined(STREAM_KERNEL_17)
   float norm[8];
-#elif defined(STREAM_KERNEL_18) || defined(STREAM_KERNEL_19)
+#elif defined(STREAM_KERNEL_18) || defined(STREAM_KERNEL_19) || defined(STREAM_KERNEL_20)
   float norm[32];
 #endif
 };
@@ -257,6 +258,14 @@ stream_kernel_18_SSE_intrinsics (const unsigned int number_stream_elements,
 #ifdef STREAM_KERNEL_19
 void
 stream_kernel_19_SSE_intrinsics (const unsigned int number_stream_elements,
+    float alpha,
+    float tolerance,
+    struct multiply_stream_t *multiply_stream);
+#endif
+
+#ifdef STREAM_KERNEL_20
+void
+stream_kernel_20_SSE_intrinsics (const unsigned int number_stream_elements,
     float alpha,
     float tolerance,
     struct multiply_stream_t *multiply_stream);
@@ -529,6 +538,9 @@ stream_multiply (const unsigned long long number_stream_elements,
 
 #elif defined(STREAM_KERNEL_19)
   stream_kernel_19_SSE_intrinsics(number_stream_elements, alpha, 1e-18, multiply_stream);
+
+#elif defined(STREAM_KERNEL_20)
+  stream_kernel_20_SSE_intrinsics(number_stream_elements, alpha, 1e-18, multiply_stream);
 
 #elif defined(POINTER_CHASE)
 
@@ -1382,7 +1394,7 @@ spamm_multiply_node (const struct matrix_node_t *A_node,
     {
       stream[*index].norm[i] = rand()/(float) RAND_MAX;
     }
-#elif defined(STREAM_KERNEL_18) || defined(STREAM_KERNEL_19)
+#elif defined(STREAM_KERNEL_18) || defined(STREAM_KERNEL_19) || defined(STREAM_KERNEL_20)
     for (i = 0; i < 32; i++)
     {
       stream[*index].norm[i] = rand()/(float) RAND_MAX;
@@ -1908,6 +1920,9 @@ main (int argc, char **argv)
 
 #elif defined(STREAM_KERNEL_19)
   printf("using stream_kernel_19\n");
+
+#elif defined(STREAM_KERNEL_20)
+  printf("using stream_kernel_20\n");
 
 #elif defined(POINTER_CHASE)
   printf("pointer chase\n");
