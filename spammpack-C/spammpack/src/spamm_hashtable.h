2d542f603dd3984cf73989318b7703d9599e0867
Nicolas Bock <nicolasbock@gmail.com>
2010-12-22 16:20:27 -0700

    Added remove() function to hashtable.
    This function removes a key from the hashtable.

--- spammpack-C/spammpack/src/spamm_hashtable.h
+++ spammpack-C/spammpack/src/spamm_hashtable.h
@@ -11,6 +11,9 @@ spamm_hash_uint_equal (gconstpointer a, gconstpointer b);
 unsigned int
 spamm_hashtable_hash_1 (const unsigned int key);
 
+guint
+spamm_hashtable_hash_1_glib (gconstpointer key);
+
 struct spamm_hashtable_t *
 spamm_hashtable_new ();
 
@@ -26,6 +29,10 @@ spamm_hashtable_insert (struct spamm_hashtable_t *hashtable,
 
 void *
 spamm_hashtable_lookup (struct spamm_hashtable_t *hashtable,
+    const unsigned int key);
+
+void *
+spamm_hashtable_remove (struct spamm_hashtable_t *hashtable,
     const unsigned int key);
 
 unsigned int
