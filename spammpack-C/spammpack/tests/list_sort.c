b7c9eb6dfbf45aac6d88ab07f187ee82dc915d57
Nicolas Bock <nicolasbock@gmail.com>
2011-01-17 20:27:57 -0700

    Fixed second quicksort implementation...
    in spamm_multiply.c. It takes unfortunately almost twice as long as the first
    implementation, which might have to do with my somewhat hackish implementation
    of getting around the fact that an "unsigned int" is, well, not signed, and
    the wikipedia people assume a signed int. I am sure one could get around this
    issue in a smarter way, but I have a hard time believing that it would speed
    up the code by that much.

--- spammpack-C/spammpack/tests/list_sort.c
+++ spammpack-C/spammpack/tests/list_sort.c
@@ -2,32 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 100
+#define N 10000
 
 int
 main ()
 {
   int result = 0;
-  int i;
+  unsigned int i;
+  unsigned int last_i;
+  unsigned int value, last_value;
   struct spamm_list_t *list;
 
   list = spamm_list_new(N);
   for (i = 0; i < N; i++)
   {
-    spamm_list_set(list, i, N-i-1);
+    spamm_list_set(list, i, rand());
   }
 
   spamm_list_sort(list, spamm_list_compare_int, NULL);
 
+  last_i = 0;
+  last_value = spamm_list_get(list, 0);
   for (i = 0; i < N; i++)
   {
-    if (spamm_list_get(list, i) != i)
+    value = spamm_list_get(list, i);
+    if (value > last_value)
+    {
+      last_value = value;
+      last_i = i;
+    }
+
+    else if (value < last_value)
     {
       result = -1;
-      printf("failed to sort, list[%u] = %i, should be %i...\n", i, spamm_list_get(list, i), i);
+      printf("failed to sort, list[%u] = %i, list[%u] = %i...\n", i, spamm_list_get(list, i), last_i, last_value);
       break;
     }
   }
+
+  spamm_list_delete(&list);
 
   return result;
 }
