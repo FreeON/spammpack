a9c940c2177821a980eefe602a8c2b03baa23149
Nicolas Bock <nicolasbock@gmail.com>
2011-03-16 17:06:40 -0600

    Added norm_upper arrays to spamm_data_t so we...
    can do hierarchical norm product checking in the kernel.

--- spammpack-C/spammpack/src/print_data_sizes.c
+++ spammpack-C/spammpack/src/print_data_sizes.c
@@ -15,10 +15,14 @@ main ()
 
   printf("sizeof(struct spamm_data_t) = %lu\n", sizeof(struct spamm_data_t));
   printf("&data at                       %p\n", &data);
-  printf("&data.norm at                  %p, offset = %lu\n", &data.norm, (unsigned long int) &data.norm - (unsigned long int) &data);
-  printf("&data.block_dense at           %p, offset = %lu\n", &data.block_dense, (unsigned long int) &data.block_dense - (unsigned long int) &data);
-  printf("&data.block_dense_dilated at   %p, offset = %lu\n", &data.block_dense_dilated, (unsigned long int) &data.block_dense_dilated - (unsigned long int) &data);
+  printf("&data.norm at                  %p, offset_norm = %lu\n", &data.norm, (unsigned long int) &data.norm - (unsigned long int) &data);
+#ifdef SPAMM_KERNEL_IMPLEMENTATION_Z_CURVE_SSE4_1
+  printf("&data.norm_upper at            %p, offset_norm_upper = %lu\n", &data.norm_upper, (unsigned long int) &data.norm_upper - (unsigned long int) &data);
+  printf("&data.norm_upper_transpose at  %p, offset_norm_upper_transpose = %lu\n", &data.norm_upper_transpose, (unsigned long int) &data.norm_upper_transpose - (unsigned long int) &data);
+#endif
+  printf("&data.block_dense at           %p, offset_block_dense = %lu\n", &data.block_dense, (unsigned long int) &data.block_dense - (unsigned long int) &data);
+  printf("&data.block_dense_dilated at   %p, offset_block_dense_dilated = %lu\n", &data.block_dense_dilated, (unsigned long int) &data.block_dense_dilated - (unsigned long int) &data);
 #ifdef SPAMM_USE_TRANSPOSE
-  printf("&data.block_dense_transpose at %p, offset = %lu\n", &data.block_dense_transpose, (unsigned long int) &data.block_dense_transpose - (unsigned long int) &data);
+  printf("&data.block_dense_transpose at %p, offset_block_dense_transpose = %lu\n", &data.block_dense_transpose, (unsigned long int) &data.block_dense_transpose - (unsigned long int) &data);
 #endif
 }
