74a7a48628b720028c274117a5088a11b6cc7d32
Nicolas Bock <nicolasbock@gmail.com>
2012-04-11 15:13:21 -0600

    More cleanup.

--- spammpack-C/spammpack/src/spamm.h
+++ spammpack-C/spammpack/src/spamm.h
@@ -4,14 +4,14 @@
 #define __SPAMM_H
 
 /* Include the other header files. */
-#include "spamm_error.h"
-#include "spamm_hashtable.h"
-#include "spamm_introspection.h"
 #include "spamm_list.h"
+#include "spamm_hashtable.h"
 #include "spamm_kernel.h"
+#include "spamm_types.h"
+#include "spamm_errno.h"
+
 #include "spamm_timer.h"
 #include "spamm_convert.h"
-#include "spamm_types.h"
 #include "spamm_general.h"
 
 #endif
