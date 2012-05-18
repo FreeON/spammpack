e8c6ad9708978fd63931d67b7acab1d65b8541df
Nicolas Bock <nicolasbock@gmail.com>
2012-05-18 16:20:33 -0600

    Moving things around in spammpack.

--- spammpack-C/spammpack/tests/multiply.c
+++ spammpack-C/spammpack/tests/multiply.c
@@ -1,5 +1,8 @@
 #include "spamm.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int
 main (int argc, char *argv[])
 {
@@ -7,11 +10,35 @@ main (int argc, char *argv[])
   const int M = 103;
   const int N = 129;
 
+  unsigned int i, j;
+  float Aij;
+
+  FILE *fd;
+
+  const char *filename_A = "testmatrix_1.coor";
+
   struct spamm_matrix_t *A;
 
+  /* Allocate matrices. */
   A = spamm_new(M, N);
 
   /* Load matrix from file. */
+  if ((fd = fopen(filename_A, "r")) == NULL)
+  {
+    printf("error opening matrix file \"%s\"\n", filename_A);
+    exit(1);
+  }
+
+  while (fscanf(fd, "%u %u %e\n", &i, &j, &Aij) == 3)
+  {
+    spamm_set(i, j, Aij, A);
+  }
+
+  if (fclose(fd) != 0)
+  {
+    printf("error closing matrix file\n");
+    exit(1);
+  }
 
   return result;
 }
