3987cabdb1ed1112695373c985225ecf6c8712f1
Nicolas Bock <nicolasbock@gmail.com>
2012-05-22 13:20:00 -0600

    Added dependencies to makefile in SpAMMPACK.

--- src/SpAMM_TIMER.c
+++ src/SpAMM_TIMER.c
@@ -1,5 +1,9 @@
-#include "IPM_timer.h"
-double spamm_ipm_get_time()   { return IPM_timer_get_time();}
-double spamm_ipm_get_time_()  { return IPM_timer_get_time();}
-double spamm_ipm_get_time__() { return IPM_timer_get_time();}
+#include <time.h>
 
+double spamm_ipm_get_time()
+{
+  return (double) time(NULL);
+}
+
+double spamm_ipm_get_time_() { return spamm_ipt_get_time(); }
+double spamm_ipm_get_time__() { return spamm_ipt_get_time(); }
