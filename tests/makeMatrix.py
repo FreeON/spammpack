a641e0d4e4d25333b95055d99077f1e97d9e6850
Nicolas Bock <nicolasbock@gmail.com>
2012-05-26 06:01:24 -0600

    Added future option for different random matrix types.

--- tests/makeMatrix.py
+++ tests/makeMatrix.py
@@ -2,6 +2,7 @@
 
 import argparse
 import random
+import sys
 
 parser = argparse.ArgumentParser()
 
@@ -10,8 +11,23 @@ parser.add_argument("-N",
     type = int,
     default = 1)
 
+parser.add_argument("--type",
+    dest = "matrixtype",
+    help = "Matrix type (default = %(default)s)",
+    choices = [ "full", "decay" ],
+    default = "full")
+
 options = parser.parse_args()
 
-for i in range(options.N):
-  for j in range(options.N):
-    print "%d %d % 1.14e" % (i+1, j+1, random.uniform(0.1, 1.0))
+if options.matrixtype == "full":
+  for i in range(options.N):
+    for j in range(options.N):
+      print "%d %d % 1.14e" % (i+1, j+1, random.uniform(0.1, 1.0))
+
+elif options.matrixtype == "decay":
+  print "not implemented"
+  sys.exit(1)
+
+else:
+  print "unknown matrix type (%s)" % (options.matrixtype)
+  sys.exit(1)
