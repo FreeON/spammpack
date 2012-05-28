0d233aa0823854d3f92819eb1544d1271109fee3
Nicolas Bock <nicolasbock@gmail.com>
2012-05-28 16:17:29 -0600

    Write all test output into one file for processing.

---
+++ tests/scaling_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+echo > scaling_test.output
+
+./spamm_multiply_01 >> scaling_test.output
+./spamm_multiply_02 >> scaling_test.output
+./spamm_multiply_04 >> scaling_test.output
+./spamm_multiply_08 >> scaling_test.output
+./spamm_multiply_12 >> scaling_test.output
+./spamm_multiply_16 >> scaling_test.output
+./spamm_multiply_20 >> scaling_test.output
+./spamm_multiply_24 >> scaling_test.output
+./spamm_multiply_28 >> scaling_test.output
+./spamm_multiply_32 >> scaling_test.output
+./spamm_multiply_36 >> scaling_test.output
+./spamm_multiply_40 >> scaling_test.output
+./spamm_multiply_44 >> scaling_test.output
+./spamm_multiply_48 >> scaling_test.output
+
+grep SpAMM_SCALING scaling_test.output > scaling_test.dat
