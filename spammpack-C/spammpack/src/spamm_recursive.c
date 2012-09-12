5818ed1ad93a1c81cbc89309a833bd1d80c42032
Nicolas Bock <nicolasbock@gmail.com>
2012-09-12 15:02:52 -0600

    Removed unecessary files.

--- spammpack-C/spammpack/src/spamm_recursive.c
+++
@@ -1,8 +0,0 @@
-#include "spamm.h"
-#include "spamm_types_private.h"
-
-#include <assert.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
--- spammpack-C/spammpack/src/spamm_recursive.h
+++
@@ -1,9 +0,0 @@
-/** @file */
-
-#ifndef __SPAMM_NAIVE_H
-#define __SPAMM_NAIVE_H
-
-#include "spamm_timer.h"
-#include "spamm_types.h"
-
-#endif
