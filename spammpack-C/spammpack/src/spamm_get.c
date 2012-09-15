72a8920ed3c49a676a6ee718d5e9de21f32c3622
Nicolas Bock <nicolasbock@gmail.com>
2012-09-15 15:56:31 -0600

    Checkpoint.

--- spammpack-C/spammpack/src/spamm_get.c
+++ spammpack-C/spammpack/src/spamm_get.c
@@ -6,74 +6,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** Get an element from a recursive matrix.
- *
- * If the matrix is NULL, this function returns 0.
- *
- * @param i The row index.
- * @param j The column index.
- * @param A The matrix.
- *
- * @return The matrix element Aij.
- */
-float
-spamm_recursive_get (const unsigned int i, const unsigned int j, const struct spamm_recursive_t *A)
-{
-  struct spamm_recursive_node_t **node = NULL;
-
-  if (A == NULL)
-  {
-    return 0;
-  }
-
-  node = (struct spamm_recursive_node_t**) &(A->root);
-
-  while (1)
-  {
-    if (*node == NULL)
-    {
-      return 0;
-    }
-
-    if ((*node)->M_upper-(*node)->M_lower == A->N_contiguous)
-    {
-      /* Get the matrix element. */
-      if ((*node)->data == NULL) { return 0.0; }
-      else
-      {
-        return (*node)->data[spamm_index_column_major(i-(*node)->M_lower, j-(*node)->N_lower, A->N_contiguous, A->N_contiguous)];
-      }
-    }
-
-    else
-    {
-      if (i < (*node)->M_lower+((*node)->M_upper-(*node)->M_lower)/2 &&
-          j < (*node)->N_lower+((*node)->N_upper-(*node)->N_lower)/2)
-      {
-        node = &((*node)->child[0]);
-      }
-
-      else if (i <  (*node)->M_lower+((*node)->M_upper-(*node)->M_lower)/2 &&
-               j >= (*node)->N_lower+((*node)->N_upper-(*node)->N_lower)/2)
-      {
-        node = &((*node)->child[1]);
-      }
-
-      else if (i >= (*node)->M_lower+((*node)->M_upper-(*node)->M_lower)/2 &&
-               j <  (*node)->N_lower+((*node)->N_upper-(*node)->N_lower)/2)
-      {
-        node = &((*node)->child[2]);
-      }
-
-      else if (i >= (*node)->M_lower+((*node)->M_upper-(*node)->M_lower)/2 &&
-               j >= (*node)->N_lower+((*node)->N_upper-(*node)->N_lower)/2)
-      {
-        node = &((*node)->child[3]);
-      }
-    }
-  }
-}
-
 /** Get an element from a matrix.
  *
  * @param i The row index.
@@ -117,7 +49,7 @@ spamm_hashed_get (const unsigned int i, const unsigned int j, const struct spamm
  * @return The matrix element Aij.
  */
 float
-spamm_recursive_get_recursive (const unsigned int i, const unsigned int j, const struct spamm_recursive_node_t *node)
+spamm_recursive_get (const unsigned int i, const unsigned int j, const struct spamm_recursive_node_t *node)
 {
   unsigned int number_rows;
   unsigned int number_columns;
@@ -147,25 +79,25 @@ spamm_recursive_get_recursive (const unsigned int i, const unsigned int j, const
     if (i < node->M_lower+(number_rows)/2 &&
         j < node->N_lower+(number_columns)/2)
     {
-      return spamm_recursive_get_recursive(i, j, node->child[0]);
+      return spamm_recursive_get(i, j, node->child[0]);
     }
 
     else if (i <  node->M_lower+(number_rows)/2 &&
         j >= node->N_lower+(number_columns)/2)
     {
-      return spamm_recursive_get_recursive(i, j, node->child[1]);
+      return spamm_recursive_get(i, j, node->child[1]);
     }
 
     else if (i >= node->M_lower+(number_rows)/2 &&
         j <  node->N_lower+(number_columns)/2)
     {
-      return spamm_recursive_get_recursive(i, j, node->child[2]);
+      return spamm_recursive_get(i, j, node->child[2]);
     }
 
     else if (i >= node->M_lower+(number_rows)/2 &&
         j >= node->N_lower+(number_columns)/2)
     {
-      return spamm_recursive_get_recursive(i, j, node->child[3]);
+      return spamm_recursive_get(i, j, node->child[3]);
     }
 
     else
@@ -209,7 +141,7 @@ spamm_get (const unsigned int i, const unsigned int j, const struct spamm_matrix
 
   else
   {
-    return spamm_recursive_get_recursive(i, j, A->recursive_tree);
+    return spamm_recursive_get(i, j, A->recursive_tree);
   }
 }
 
