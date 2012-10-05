051bfb2195a6a944cbed59e78e4333b9488fa48b
Nicolas Bock <nicolasbock@gmail.com>
2012-10-05 13:50:05 -0600

    Updated sparse add test.

--- spammpack-C/spammpack/tests/add_spamm_sparse.c
+++ spammpack-C/spammpack/tests/add_spamm_sparse.c
@@ -7,9 +7,6 @@
 #define TEST_TOLERANCE 5e-7
 #define DECAY 0.7
 
-//#define A_SPARSE 1
-#define B_SPARSE 1
-
 //#define PRINT_MATRIX
 
 int
@@ -34,95 +31,112 @@ main (int argc, char **argv)
 
   int i, j;
 
-  A_dense = calloc(N*N, sizeof(double));
-  B_dense = calloc(N*N, sizeof(double));
+  short sparse_test;
+  const short sparse_A[] = { 0, 0, 1, 1 };
+  const short sparse_B[] = { 0, 1, 0, 1 };
 
-  for (i = 0; i < N; i++)
+  for (sparse_test = 0; sparse_test < 4; sparse_test++)
   {
-    A_dense[spamm_index_row_major(i, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
-    B_dense[spamm_index_row_major(i, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
+    A_dense = calloc(N*N, sizeof(double));
+    B_dense = calloc(N*N, sizeof(double));
 
-    for (j = i+1; j < N; j++)
+    for (i = 0; i < N; i++)
     {
-      /* Exponential decay. */
-#ifdef A_SPARSE
-      A_dense[spamm_index_row_major(i, j, N, N)] = A_dense[spamm_index_row_major(i, i, N, N)]*exp(-fabsf(i-j)/DECAY);
-      A_dense[spamm_index_row_major(j, i, N, N)] = A_dense[spamm_index_row_major(i, j, N, N)];
-#else
-      A_dense[spamm_index_row_major(i, j, N, N)] = 0.8+rand()/(double) RAND_MAX;
-      A_dense[spamm_index_row_major(j, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
-#endif
+      A_dense[spamm_index_row_major(i, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
+      B_dense[spamm_index_row_major(i, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
 
-#ifdef B_SPARSE
-      B_dense[spamm_index_row_major(i, j, N, N)] = B_dense[spamm_index_row_major(i, i, N, N)]*exp(-fabsf(i-j)/DECAY);
-      B_dense[spamm_index_row_major(j, i, N, N)] = B_dense[spamm_index_row_major(i, j, N, N)];
-#else
-      B_dense[spamm_index_row_major(i, j, N, N)] = 0.8+rand()/(double) RAND_MAX;
-      B_dense[spamm_index_row_major(j, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
-#endif
-    }
-  }
-
-#ifdef PRINT_MATRIX
-  printf("A_dense =\n");
-  spamm_print_dense(N, N, row_major, A_dense);
-
-  printf("B_dense =\n");
-  spamm_print_dense(N, N, row_major, B_dense);
-#endif
-
-  /* Convert to SpAMM matrix. */
-  A = spamm_convert_dense_to_spamm(N, N, linear_tier, contiguous_tier, row_major, A_dense, row_major);
-  B = spamm_convert_dense_to_spamm(N, N, linear_tier, contiguous_tier, row_major, B_dense, row_major);
-
-  /* Add by hand for verification. */
-  for (i = 0; i < N; i++) {
-    for (j = 0; j < N; j++)
-    {
-      A_dense[spamm_index_row_major(i, j, N, N)] =
-        alpha*A_dense[spamm_index_row_major(i, j, N, N)]
-        + beta*B_dense[spamm_index_row_major(i, j, N, N)];
-    }
-  }
-
-#ifdef PRINT_MATRIX
-  printf("A_dense =\n");
-  spamm_print_dense(N, N, row_major, A_dense);
-#endif
-
-  spamm_add(alpha, A, beta, B);
-
-#ifdef PRINT_MATRIX
-  printf("A =\n");
-  spamm_print(A);
-#endif
-
-  /* Check tree consistency. */
-  result = spamm_check(A, TEST_TOLERANCE);
-
-  /* Compare result. */
-  max_diff = 0.0;
-  for (i = 0; i < N; i++) {
-    for (j = 0; j < N; j++)
-    {
-      if (fabs(A_dense[spamm_index_row_major(i, j, N, N)]-spamm_get(i, j, A)) > max_diff)
+      for (j = i+1; j < N; j++)
       {
-        max_diff = fabs(A_dense[spamm_index_row_major(i, j, N, N)]-spamm_get(i, j, A));
+        /* Exponential decay. */
+        if (sparse_A[sparse_test] == 1)
+        {
+          A_dense[spamm_index_row_major(i, j, N, N)] = A_dense[spamm_index_row_major(i, i, N, N)]*exp(-fabsf(i-j)/DECAY);
+          A_dense[spamm_index_row_major(j, i, N, N)] = A_dense[spamm_index_row_major(i, j, N, N)];
+        }
+
+        else
+        {
+          A_dense[spamm_index_row_major(i, j, N, N)] = 0.8+rand()/(double) RAND_MAX;
+          A_dense[spamm_index_row_major(j, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
+        }
+
+        if (sparse_B[sparse_test] == 1)
+        {
+          B_dense[spamm_index_row_major(i, j, N, N)] = B_dense[spamm_index_row_major(i, i, N, N)]*exp(-fabsf(i-j)/DECAY);
+          B_dense[spamm_index_row_major(j, i, N, N)] = B_dense[spamm_index_row_major(i, j, N, N)];
+        }
+
+        else
+        {
+          B_dense[spamm_index_row_major(i, j, N, N)] = 0.8+rand()/(double) RAND_MAX;
+          B_dense[spamm_index_row_major(j, i, N, N)] = 0.8+rand()/(double) RAND_MAX;
+        }
       }
     }
+
+#ifdef PRINT_MATRIX
+    printf("A_dense =\n");
+    spamm_print_dense(N, N, row_major, A_dense);
+
+    printf("B_dense =\n");
+    spamm_print_dense(N, N, row_major, B_dense);
+#endif
+
+    /* Convert to SpAMM matrix. */
+    A = spamm_convert_dense_to_spamm(N, N, linear_tier, contiguous_tier, row_major, A_dense, row_major);
+    B = spamm_convert_dense_to_spamm(N, N, linear_tier, contiguous_tier, row_major, B_dense, row_major);
+
+    /* Add by hand for verification. */
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++)
+      {
+        A_dense[spamm_index_row_major(i, j, N, N)] =
+          alpha*A_dense[spamm_index_row_major(i, j, N, N)]
+          + beta*B_dense[spamm_index_row_major(i, j, N, N)];
+      }
+    }
+
+#ifdef PRINT_MATRIX
+    printf("A_dense =\n");
+    spamm_print_dense(N, N, row_major, A_dense);
+#endif
+
+    spamm_add(alpha, A, beta, B);
+
+#ifdef PRINT_MATRIX
+    printf("A =\n");
+    spamm_print(A);
+#endif
+
+    /* Check tree consistency. */
+    result = spamm_check(A, TEST_TOLERANCE);
+
+    /* Compare result. */
+    max_diff = 0.0;
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++)
+      {
+        if (fabs(A_dense[spamm_index_row_major(i, j, N, N)]-spamm_get(i, j, A)) > max_diff)
+        {
+          max_diff = fabs(A_dense[spamm_index_row_major(i, j, N, N)]-spamm_get(i, j, A));
+        }
+      }
+    }
+
+    /* Test result. */
+    if (max_diff > TEST_TOLERANCE)
+    {
+      result = SPAMM_ERROR;
+      printf("[add_spamm] max diff (sparse_A = %u, sparse_B = %u, test tolerance was %1.2e) = %e\n",
+          sparse_A[sparse_test], sparse_B[sparse_test], TEST_TOLERANCE, max_diff);
+    }
+
+    free(A_dense);
+    free(B_dense);
+
+    spamm_delete(&A);
+    spamm_delete(&B);
   }
-
-  if (max_diff > TEST_TOLERANCE)
-  {
-    result = SPAMM_ERROR;
-    printf("[add_spamm] max diff (test tolerance was %1.2e) = %e\n", TEST_TOLERANCE, max_diff);
-  }
-
-  free(A_dense);
-  free(B_dense);
-
-  spamm_delete(&A);
-  spamm_delete(&B);
 
   return result;
 }
