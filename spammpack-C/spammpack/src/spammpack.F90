ac53e6d2e00dac3b034e2be29bd02117ebc93c71
Nicolas Bock <nicolasbock@gmail.com>
2012-10-22 11:10:09 -0600

    Added documentation for Fortran spammpack module.

--- spammpack-C/spammpack/src/spammpack.F90
+++ spammpack-C/spammpack/src/spammpack.F90
@@ -1,3 +1,5 @@
+!> @file
+
 module spammpack
 
   use spamm_derived
@@ -6,22 +8,26 @@ module spammpack
 
   interface
 
+    !> Interface for spamm_new_chunk().
     subroutine spamm_new_chunk (ndim, N_contiguous, chunk) bind(C, name = "spamm_new_chunk_interface")
       use, intrinsic :: iso_c_binding
       integer(c_int), intent(in) :: ndim, N_contiguous
       type(c_ptr) :: chunk
     end subroutine spamm_new_chunk
 
+    !> Interface for spamm_chunk_get_N_lower().
     subroutine spamm_chunk_get_N_lower (cptr, chunk) bind(C, name = "spamm_chunk_get_N_lower_interface")
       use, intrinsic :: iso_C_binding
       type(c_ptr) :: chunk, cptr
     end subroutine spamm_chunk_get_N_lower
 
+    !> Interface for spamm_chunk_get_N_upper().
     subroutine spamm_chunk_get_N_upper (cptr, chunk) bind(C, name = "spamm_chunk_get_N_upper_interface")
       use, intrinsic :: iso_C_binding
       type(c_ptr) :: chunk, cptr
     end subroutine spamm_chunk_get_N_upper
 
+    !> Interface for spamm_chunk_print().
     subroutine spamm_chunk_print (chunk) bind(C, name = "spamm_chunk_print_interface")
       use, intrinsic :: iso_C_binding
       type(c_ptr) :: chunk
