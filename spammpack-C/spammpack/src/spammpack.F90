58276e77aac0c600f3486eda294bbf9a3cbbb00b
Nicolas Bock <nicolasbock@gmail.com>
2012-10-22 10:51:55 -0600

    Checkpoint.

--- spammpack-C/spammpack/src/spammpack.F90
+++ spammpack-C/spammpack/src/spammpack.F90
@@ -1,6 +1,33 @@
 module spammpack
 
+  use spamm_derived
+
   IMPLICIT NONE
+
+  interface
+
+    subroutine spamm_new_chunk (ndim, N_contiguous, chunk) bind(C, name = "spamm_new_chunk_interface")
+      use, intrinsic :: iso_c_binding
+      integer(c_int), intent(in) :: ndim, N_contiguous
+      type(c_ptr) :: chunk
+    end subroutine spamm_new_chunk
+
+    subroutine spamm_chunk_get_N_lower (cptr, chunk) bind(C, name = "spamm_chunk_get_N_lower_interface")
+      use, intrinsic :: iso_C_binding
+      type(c_ptr) :: chunk, cptr
+    end subroutine spamm_chunk_get_N_lower
+
+    subroutine spamm_chunk_get_N_upper (cptr, chunk) bind(C, name = "spamm_chunk_get_N_upper_interface")
+      use, intrinsic :: iso_C_binding
+      type(c_ptr) :: chunk, cptr
+    end subroutine spamm_chunk_get_N_upper
+
+    subroutine spamm_chunk_print (chunk) bind(C, name = "spamm_chunk_print_interface")
+      use, intrinsic :: iso_C_binding
+      type(c_ptr) :: chunk
+    end subroutine spamm_chunk_print
+
+  end interface
 
 contains
 
--- spammpack-C/spammpack/tests/chunk_test.F90
+++ spammpack-C/spammpack/tests/chunk_test.F90
@@ -1,31 +1,7 @@
 program chunk_test
 
+  use :: spammpack
   use, intrinsic :: iso_c_binding
-
-  interface
-
-    subroutine spamm_new_chunk (ndim, N_contiguous, chunk) bind(C, name = "spamm_new_chunk_interface")
-      use, intrinsic :: iso_c_binding
-      integer, intent(in) :: ndim, N_contiguous
-      type(c_ptr) :: chunk
-    end subroutine spamm_new_chunk
-
-    subroutine spamm_chunk_get_N_lower (cptr, chunk) bind(C, name = "spamm_chunk_get_N_lower_interface")
-      use, intrinsic :: iso_C_binding
-      type(c_ptr) :: chunk, cptr
-    end subroutine spamm_chunk_get_N_lower
-
-    subroutine spamm_chunk_get_N_upper (cptr, chunk) bind(C, name = "spamm_chunk_get_N_upper_interface")
-      use, intrinsic :: iso_C_binding
-      type(c_ptr) :: chunk, cptr
-    end subroutine spamm_chunk_get_N_upper
-
-    subroutine spamm_chunk_print (chunk) bind(C, name = "spamm_chunk_print_interface")
-      use, intrinsic :: iso_C_binding
-      type(c_ptr) :: chunk
-    end subroutine spamm_chunk_print
-
-  end interface
 
   INTEGER :: number_dimensions = 2
   INTEGER :: N_contiguous = 128
