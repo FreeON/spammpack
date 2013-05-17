117fac716f24bd8b5a28bfb20eccf2989b345062
Nicolas Bock <nicolasbock@gmail.com>
2013-05-17 10:41:53 -0600

    Added tool to verify chunk indexing.

---
+++ spammpack-C/spammpack/src/get_chunk_index.c
@@ -0,0 +1,34 @@
+#include "spamm.h"
+
+#include <stdio.h>
+
+void
+main ()
+{
+  int i[2];
+  const unsigned int N_lower[2] = { 0, 0 };
+  const unsigned int N_upper[2] = { 16, 16 };
+  unsigned int offset;
+
+  SPAMM_INFO("N_lower = { %u, %u }\n", N_lower[0], N_lower[1]);
+  SPAMM_INFO("N_upper = { %u, %u }\n", N_upper[0], N_upper[1]);
+
+  while(1)
+  {
+    printf("Enter i j: "); fflush(stdout);
+
+    if(fscanf(stdin, "%i %i", &i[0], &i[1]) != 2)
+    {
+      break;
+    }
+
+    if(i[0] < 0 || i[1] < 0)
+    {
+      break;
+    }
+
+    offset = spamm_chunk_matrix_index(2, 1, N_lower, N_upper, i);
+    SPAMM_INFO("i = %i, j = %i, chunk_offset = %u, # kernel blocks = %u\n", i[0],
+        i[1], offset, offset/(SPAMM_N_KERNEL*SPAMM_N_KERNEL));
+  }
+}
