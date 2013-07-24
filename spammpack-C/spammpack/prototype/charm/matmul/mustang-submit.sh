0783ac3f3e1e687cac32449661d5f7406e41d21d
Nicolas Bock <nicolasbock@gmail.com>
2013-07-24 14:19:58 -0600

    Fixed mustang job scripts a bit.

--- spammpack-C/spammpack/prototype/charm/matmul/mustang-submit.sh
+++ spammpack-C/spammpack/prototype/charm/matmul/mustang-submit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-for i in 1 2 4 8 12 16 20; do
+for i in 1 2 4 8 12 16 20 24 28 32 36 40; do
   sed -e "s:nodes=[0-9]\+:nodes=${i}:" \
     -e "s:matmul-[0-9]\+:matmul-${i}:" \
-    mustang.job > msub
+    mustang.job.template > mustang.job
+  msub mustang.job
+  rm -f mustang.job
 done
