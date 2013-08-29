d03e050f223f215b956136bd36042057533b114d
Nicolas Bock <nicolasbock@gmail.com>
2013-08-29 13:39:29 -0600

    Small update to job submission script.

--- spammpack-C/spammpack/prototype/charm/matmul/mustang-submit.sh
+++ spammpack-C/spammpack/prototype/charm/matmul/mustang-submit.sh
@@ -5,17 +5,17 @@ NODES=( 1 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 96 128 160 192 224 256
 for i in ${NODES[@]}; do
 
   sed -e "s:nodes=[0-9]\+:nodes=${i}:" \
-    -e "s:MM-[0-9]\+:MM-${i}:" \
+    -e "s:MM-[0-9]\+:MM-full-${i}:" \
     mustang.job.full.template > mustang.job
   msub mustang.job
 
   sed -e "s:nodes=[0-9]\+:nodes=${i}:" \
-    -e "s:MM-[0-9]\+:MM-${i}:" \
+    -e "s:MM-[0-9]\+:MM-decay-${i}:" \
     mustang.job.decay.template > mustang.job
   msub mustang.job
 
   sed -e "s:nodes=[0-9]\+:nodes=${i}:" \
-    -e "s:MM-[0-9]\+:MM-${i}:" \
+    -e "s:MM-[0-9]\+:MM-diagonal-${i}:" \
     mustang.job.diagonal.template > mustang.job
   msub mustang.job
 
