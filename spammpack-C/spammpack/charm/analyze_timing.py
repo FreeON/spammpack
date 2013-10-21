b18e7ed1906e796dc45e6f9cf3d020ad8043ded5
Nicolas Bock <nicolasbock@gmail.com>
2013-10-21 17:06:25 -0600

    Another update to analyze_timing.py

--- spammpack-C/spammpack/charm/analyze_timing.py
+++ spammpack-C/spammpack/charm/analyze_timing.py
@@ -9,39 +9,50 @@ import signal
 import sys
 
 def make_graph ():
-  for PPV in timing:
-    np = []
-    for i in timing[PPV]:
-      np.append(i)
-    np.sort()
+  np = []
+  for i in timing[PPV]:
+    np.append(i)
+  np.sort()
 
-    t = []
-    np_dead = []
-    if iteration < 0:
-      additional_label = "all"
-      for i in np:
-        t.append(timing[PPV][i]["t_total"])
-    else:
-      additional_label = "iteration {:d}".format(iteration)
-      for i in np:
-        if iteration <= len(timing[PPV][i]["t_multiply"]):
-          t.append(
-              timing[PPV][i]["t_multiply"][iteration-1]
-              +timing[PPV][i]["t_add"][iteration-1]
-              +timing[PPV][i]["t_setEq"][iteration-1])
-        else:
-          np_dead.append(i)
+  t = []
+  np_dead = []
 
-    for i in np_dead:
-      np.remove(i)
+  global t_0
+  global number_samples
 
-    plt.loglog(np, t, '-o', label = "PPV{:03d} ({:s})".format(
-      PPV, additional_label), hold = True)
+  if iteration < 0:
+    additional_label = "all"
+    for i in np:
+      t.append(timing[PPV][i]["t_total"])
+  else:
+    for i in np:
+      if iteration <= len(timing[PPV][i]["t_multiply"]):
+        t.append(
+            timing[PPV][i]["t_multiply"][iteration-1]
+            +timing[PPV][i]["t_add"][iteration-1]
+            +timing[PPV][i]["t_setEq"][iteration-1])
+      else:
+        np_dead.append(i)
+
+  for i in np_dead:
+    np.remove(i)
+
+  additional_label = "iteration {:d}, complexity {:d}".format(
+      iteration, timing[PPV][np[0]]["complexity"][iteration-1])
+  plt.loglog(np, t, '-o', label = "PPV{:03d} ({:s})".format(
+    PPV, additional_label), hold = True)
+
+  if options.one_ideal:
+    number_samples += 1
+    t_0 += t[0]/np[0]
+  else:
     plt.loglog(
         [ np[0], np[-1] ],
-        [ t[0], t[0]/np[-1] ],
+        [ t[0], t[0]*np[0]/np[-1] ],
         '-',
         hold = True)
+
+  return np, t
 
 parser = argparse.ArgumentParser()
 
@@ -60,6 +71,15 @@ parser.add_argument("--one-graph",
     help = "Plot all iterations on one graph",
     action = "store_true",
     default = False)
+
+parser.add_argument("--one-ideal",
+    help = "average serial time to get one ideal",
+    action = "store_true",
+    default = False)
+
+parser.add_argument("--title",
+    help = "The graph title",
+    default = "")
 
 parser.add_argument("--debug",
     help = "Print debug information",
@@ -91,6 +111,7 @@ for f in options.OUTFILE:
   t_multiply = []
   t_add = []
   t_setEq = []
+  complexity = []
   for line in fd:
     if np == 0:
       result = re.compile("Running on\s+([0-9]+)\s+unique").search(line)
@@ -108,12 +129,14 @@ for f in options.OUTFILE:
         "iteration\s+([0-9]+), "
         + "multiply: ([0-9.]+) seconds, "
         + "setEq: ([0-9.]+) seconds, "
-        + "add: ([0-9.]+) seconds").search(line)
+        + "add: ([0-9.]+) seconds.+"
+        + "complexity ([0-9]+)").search(line)
     if result:
       iteration = int(result.group(1))
       t_multiply.append(float(result.group(2)))
       t_add.append(float(result.group(3)))
       t_setEq.append(float(result.group(4)))
+      complexity.append(int(result.group(5)))
 
       if iteration != len(t_multiply):
         raise Exception("strange...")
@@ -126,16 +149,44 @@ for f in options.OUTFILE:
       "t_total" : numpy.sum(t_multiply)+numpy.sum(t_add)+numpy.sum(t_setEq),
       "t_multiply" : t_multiply,
       "t_add" : t_add,
-      "t_setEq" : t_setEq
+      "t_setEq" : t_setEq,
+      "complexity" : complexity
       }
 
   if options.debug:
     print("file {:s}, PPV{:03d}, np = {:d}, {:d} iterations".format(
       f, PPV, np, len(timing[PPV][np]["t_multiply"])))
 
-pid = []
-for iteration in options.iteration:
-  if not options.one_graph:
+if options.one_graph:
+  for PPV in timing:
+    number_samples = 0
+    t_0 = 0
+
+    for iteration in options.iteration:
+      np, t = make_graph()
+      if not options.one_ideal:
+        plt.loglog(
+            [ np[0], np[-1] ],
+            [ t[0], t[0]*np[0]/np[-1] ],
+            '-',
+            hold = True)
+
+    if options.one_ideal:
+      plt.loglog(
+          [ np[0], np[-1] ],
+          [ t_0/number_samples*np[0], t_0/number_samples*np[0]/np[-1] ],
+          '-',
+          hold = True)
+
+  plt.legend()
+  plt.xlabel("# PEs")
+  plt.ylabel("walltime [s]")
+  plt.title(options.title)
+  plt.show()
+
+else:
+  pid = []
+  for iteration in options.iteration:
     pid.append(os.fork())
 
     if(pid[-1]):
@@ -143,19 +194,14 @@ for iteration in options.iteration:
 
     else:
       plt.figure(iteration)
-      make_graph()
+      for PPV in timing:
+        make_graph()
       plt.legend()
+      plt.xlabel("# PEs")
+      plt.ylabel("walltime [s]")
+      plt.title(options.title)
       plt.show()
 
-  else:
-    make_graph()
-
-
-if options.one_graph:
-  plt.legend()
-  plt.show()
-
-else:
   print("done, please press Enter to close all windows")
   sys.stdin.readline()
 
