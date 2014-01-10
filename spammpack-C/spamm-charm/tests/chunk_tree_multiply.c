1c0fe4048257a581f8f11310582ba91209c38bab
Nicolas Bock <nicolasbock@gmail.com>
2014-01-10 13:23:42 -0700

    Make verification of matrix optional.

--- spammpack-C/spamm-charm/tests/chunk_tree_multiply.c
+++ spammpack-C/spamm-charm/tests/chunk_tree_multiply.c
@@ -26,11 +26,12 @@ main (int argc, char **argv)
 
   short print_complexity = 0;
   short print_matrix = 0;
+  short verify = 1;
 
   double tolerance = 0;
 
   int c;
-  const char short_options[] = "hT:ct:N:b:p";
+  const char short_options[] = "hT:ct:N:b:pv";
   const struct option long_options[] = {
     { "help", no_argument, NULL, 'h' },
     { "type", required_argument, NULL, 'T' },
@@ -39,6 +40,7 @@ main (int argc, char **argv)
     { "N_chunk", required_argument, NULL, 'N' },
     { "N_basic", required_argument, NULL, 'b' },
     { "print", no_argument, NULL, 'p' },
+    { "no-verify", no_argument, NULL, 'v' },
     { NULL, 0, NULL, 0 }
   };
 
@@ -57,6 +59,7 @@ main (int argc, char **argv)
         printf("{ -N | --N_chunk } N      The matrix size, N_chunk\n");
         printf("{ -b | --N_basic } N      The basic sub-matrix size, N_basic\n");
         printf("{ -p | --print }          Print matrices\n");
+        printf("{ -v | --no-verify }      Do not verify result\n");
         exit(0);
         break;
 
@@ -97,6 +100,10 @@ main (int argc, char **argv)
 
       case 'p':
         print_matrix = 1;
+        break;
+
+      case 'v':
+        verify = 0;
         break;
 
       default:
@@ -163,83 +170,89 @@ main (int argc, char **argv)
   clock_gettime(CLOCKTYPE, &end_time);
 
   printf("done multiplying %dx%d chunk with %dx%d basic blocks, "
-      "tolerance %1.2e, %1.2f seconds, verifying...\n",
+      "tolerance %1.2e, %1.2f seconds\n",
       N_chunk, N_chunk,
       N_basic, N_basic,
       tolerance,
       (end_time.tv_sec+end_time.tv_nsec/1.0e9)-
       (start_time.tv_sec+start_time.tv_nsec/1.0e9));
 
-  double *C_dense = chunk_tree_to_dense(C);
-
-  if(print_matrix)
+  if(verify)
   {
-    printf("C:\n");
-    for(int i = 0; i < N; i++)
-    {
-      for(int j = 0; j < N; j++)
-      {
-        printf(" % 1.3f", C_dense[i+j*N_chunk]);
-      }
-      printf("\n");
-    }
-  }
+    printf("verifying...\n");
 
-  if(print_complexity)
-  {
-    int complexity = 0;
-    for(int i = 0; i < N_chunk/N_basic; i++)
+    double *C_dense = chunk_tree_to_dense(C);
+
+    if(print_matrix)
     {
-      for(int j = 0; j < N_chunk/N_basic; j++)
+      printf("C:\n");
+      for(int i = 0; i < N; i++)
       {
-        for(int k = 0; k < N_chunk/N_basic; k++)
+        for(int j = 0; j < N; j++)
         {
-          double norm_A = 0;
-          double norm_B = 0;
-          for(int i_basic = 0; i_basic < N_basic; i_basic++)
+          printf(" % 1.3f", C_dense[i+j*N_chunk]);
+        }
+        printf("\n");
+      }
+    }
+
+    if(print_complexity)
+    {
+      int complexity = 0;
+      for(int i = 0; i < N_chunk/N_basic; i++)
+      {
+        for(int j = 0; j < N_chunk/N_basic; j++)
+        {
+          for(int k = 0; k < N_chunk/N_basic; k++)
           {
-            for(int j_basic = 0; j_basic < N_basic; j_basic++)
+            double norm_A = 0;
+            double norm_B = 0;
+            for(int i_basic = 0; i_basic < N_basic; i_basic++)
             {
-              norm_A += SQUARE(A_dense[(i+i_basic*N_basic)*N_chunk+(k+j_basic*N_basic)]);
-              norm_B += SQUARE(A_dense[(k+i_basic*N_basic)*N_chunk+(j+j_basic*N_basic)]);
+              for(int j_basic = 0; j_basic < N_basic; j_basic++)
+              {
+                norm_A += SQUARE(A_dense[(i+i_basic*N_basic)*N_chunk+(k+j_basic*N_basic)]);
+                norm_B += SQUARE(A_dense[(k+i_basic*N_basic)*N_chunk+(j+j_basic*N_basic)]);
+              }
+            }
+
+            if(norm_A*norm_B > tolerance)
+            {
+              complexity++;
             }
           }
+        }
+      }
 
-          if(norm_A*norm_B > tolerance)
-          {
-            complexity++;
-          }
+      printf("product complexity = %d out of %d\n",
+          complexity, CUBED(N_chunk/N_basic));
+    }
+
+    double C_exact;
+    for(int i = 0; i < N_chunk; i++)
+    {
+      for(int j = 0; j < N_chunk; j++)
+      {
+        C_exact = 0;
+        for(int k = 0; k < N_chunk; k++)
+        {
+          C_exact += A_dense[i+k*N_chunk]*A_dense[k+j*N_chunk];
+        }
+
+        if(fabs(C_exact-C_dense[i+j*N_chunk]) > 1e-10)
+        {
+          printf("mismatch C[%d][%d]\n", i, j);
+          return -1;
         }
       }
     }
 
-    printf("product complexity = %d out of %d\n",
-        complexity, CUBED(N_chunk/N_basic));
+    printf("matrices are identical\n");
+
+    free(C_dense);
   }
-
-  double C_exact;
-  for(int i = 0; i < N_chunk; i++)
-  {
-    for(int j = 0; j < N_chunk; j++)
-    {
-      C_exact = 0;
-      for(int k = 0; k < N_chunk; k++)
-      {
-        C_exact += A_dense[i+k*N_chunk]*A_dense[k+j*N_chunk];
-      }
-
-      if(fabs(C_exact-C_dense[i+j*N_chunk]) > 1e-10)
-      {
-        printf("mismatch C[%d][%d]\n", i, j);
-        return -1;
-      }
-    }
-  }
-
-  printf("matrices are identical\n");
 
   free(A_dense);
-  free(C_dense);
   free(A);
   free(C);
 
