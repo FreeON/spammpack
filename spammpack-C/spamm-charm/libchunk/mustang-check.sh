a28d15cd5db02db08a8b2233eeb8b8c03c0d8198
Nicolas Bock <nicolasbock@gmail.com>
2014-01-27 14:09:57 -0700

    Added configure line for mustang.

---
+++ spammpack-C/spamm-charm/libchunk/mustang-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module purge
+module load intel
+module load mkl
+
+./configure \
+  --enable-block-multiply=blas \
+  CC=icc \
+  CPPFLAGS="-I${MKLROOT}/include" \
+  LDFLAGS="-L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm"
