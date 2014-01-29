2aef9829b5bf5f5726fd73e631afcd0aad8e156d
Nicolas Bock <nicolasbock@gmail.com>
2014-01-29 15:35:50 -0700

    Updated gnuplot files.

---
+++ spammpack-C/spamm-charm/libchunk/tests/process_scaling_data.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+class scaling_data:
+
+  def __init__ (self):
+    self.data = []
+
+  def append (self):
+    self.data.append({})
+
+  def set_lambda (self, l):
+    self.data[-1]["lambda"] = l
+
+  def set_complexity (self, c):
+    self.data[-1]["complexity"] = c
+
+  def set_threads (self, P):
+    self.data[-1]["threads"] = P
+
+  def set_tolerance (self, t):
+    self.data[-1]["tolerance"] = t
+
+  def set_walltime (self, T):
+    self.data[-1]["walltime"] = T
+
+  def __str__ (self):
+    return str(self.data)
+
+  def get_complexity (self):
+    c = []
+    for i in self.data:
+      print(i)
+      if not i["complexity"] in c:
+        c.append(i["complexity"])
+    return c
+
+def main ():
+  import argparse
+  import re
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("FILE",
+      help = "The output file of the scaling script")
+
+  options = parser.parse_args()
+
+  data = scaling_data()
+  fd = open(options.FILE)
+  for line in fd:
+    if re.compile("running:").search(line):
+      data.append()
+
+    result = re.compile("lambda = (.*)$").search(line)
+    if result:
+      data.set_lambda(float(result.group(1)))
+
+    result = re.compile("complexity ratio = (.*)$").search(line)
+    if result:
+      data.set_complexity(float(result.group(1)))
+
+    result = re.compile("using ([0-9]*) OpenMP.*tolerance (.*), (.*) seconds").search(line)
+    if result:
+      data.set_threads(int(result.group(1)))
+      data.set_tolerance(float(result.group(2)))
+      data.set_walltime(float(result.group(3)))
+
+  complexity = data.get_complexity()
+  print(complexity)
+
+if __name__ == "__main__":
+  main()
--- spammpack-C/spamm-charm/libchunk/tests/scaling-freeon.sh
+++ spammpack-C/spamm-charm/libchunk/tests/scaling-freeon.sh
@@ -5,17 +5,14 @@ OPTIONS=(
     "-b 64"
     "-T exp_decay"
     "-l 0.995"
+    "-c"
     "-v"
     )
 
 TOLERANCE=( 0 1e-8 1e-7 1e-6 )
-THREADS=( 1 2 4 8 12 16 32 40 )
+THREADS=( 48 1 2 4 8 12 16 32 40 )
 
 for tolerance in ${TOLERANCE[@]}; do
-  # Get product complexity.
-  numactl --interleave=all \
-    ./chunk_multiply ${OPTIONS[*]} --tolerance ${tolerance} -c | tee --append scaling.output
-
   for P in ${THREADS[@]}; do
     OMP_NUM_THREADS=${P} \
       numactl --interleave=all -- \
