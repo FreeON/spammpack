4ea4b1f8a127b7a49bad0b9a482f98c54aece053
Nicolas Bock <nicolasbock@gmail.com>
2014-01-29 14:26:44 -0700

    Updated configure-mustang.sh script.

--- spammpack-C/spamm-charm/libchunk/configure-mustang.sh
+++ spammpack-C/spamm-charm/libchunk/configure-mustang.sh
@@ -6,6 +6,7 @@ module load mkl
 
 ./configure \
   --enable-block-multiply=blas \
+  --disable-assert \
   CC=icc \
   CPPFLAGS="-I${MKLROOT}/include" \
   LDFLAGS="-L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm"
