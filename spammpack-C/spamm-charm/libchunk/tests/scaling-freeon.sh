bed8312bf63c43ca3ce3aa101c0b0ef3c956e7f4
Nicolas Bock <nicolasbock@gmail.com>
2014-01-31 12:59:55 -0700

    New scaling run.

--- spammpack-C/spamm-charm/libchunk/tests/scaling-freeon.sh
+++ spammpack-C/spamm-charm/libchunk/tests/scaling-freeon.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 OPTIONS=(
-    "-N 4096"
-    "-b 64"
+    "-N 8192"
+    "-b 128"
     "-T exp_decay"
-    "-l 0.995"
+    "-l 0.994"
     "-c"
     "-v"
     )
 
-TOLERANCE=( 0 1e-8 1e-7 1e-6 )
+TOLERANCE=( 0 1e-8 1e-7 1e-6 1e-5 1e-4 1e-3 )
 THREADS=( 48 1 2 4 8 12 16 32 40 )
 
 for tolerance in ${TOLERANCE[@]}; do
@@ -17,9 +17,9 @@ for tolerance in ${TOLERANCE[@]}; do
     OMP_NUM_THREADS=${P} \
       numactl --interleave=all -- \
       ./chunk_multiply ${OPTIONS[*]} --tolerance ${tolerance} \
-      | tee --append scaling.output
+      | tee --append scaling_OpenMP.output
   done
 done
 
-grep "done multiplying" scaling.output \
+grep "done multiplying" scaling_OpenMP.output \
   | sed -e "s/^.* \([0-9]\+\) OpenMP.*, \(.*\) seconds/\1:\2,/"
