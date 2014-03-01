6375b7db3630e47851b2601bdfd262bb3e71a610
Nicolas Bock <nicolasbock@gmail.com>
2014-02-28 17:29:28 -0700

    Added the other water systems.

--- spammpack-C/spamm-charm/src/water_scaling_1.sh
+++ spammpack-C/spamm-charm/src/water_scaling_1.sh
@@ -1,14 +1,81 @@
 #!/bin/bash
 
+#./spamm-charm \
+#  --block 256 \
+#  --basic 4 \
+#  --tolerance 0 \
+#  --operation SP2 \
+#  --fockian ~/data-sets/water/h2o_10.F_DIIS \
+#  --density ~/data-sets/water/h2o_10.OrthoD \
+#  --Ne 100 \
+#  --iterations 100
+
 ./spamm-charm \
-  --block 250 \
-  --basic 250 \
+  --block 750 \
+  --basic 750 \
   --tolerance 0 \
   --operation SP2 \
-  --fockian ~/data-sets/water/h2o_10.F_DIIS \
-  --density ~/data-sets/water/h2o_10.OrthoD \
-  --Ne 100 \
+  --fockian ~/data-sets/water/h2o_30.F_DIIS \
+  --density ~/data-sets/water/h2o_30.OrthoD \
+  --Ne 300 \
   --iterations 100
 
-#  --F-min -1.919805e+01
-#  --F-max 4.192886e+00
+./spamm-charm \
+  --block 1250 \
+  --basic 1250 \
+  --tolerance 0 \
+  --operation SP2 \
+  --fockian ~/data-sets/water/h2o_50.F_DIIS \
+  --density ~/data-sets/water/h2o_50.OrthoD \
+  --Ne 500 \
+  --iterations 100
+
+./spamm-charm \
+  --block 1750 \
+  --basic 1750 \
+  --tolerance 0 \
+  --operation SP2 \
+  --fockian ~/data-sets/water/h2o_70.F_DIIS \
+  --density ~/data-sets/water/h2o_70.OrthoD \
+  --Ne 700 \
+  --iterations 100
+
+./spamm-charm \
+  --block 2250 \
+  --basic 2250 \
+  --tolerance 0 \
+  --operation SP2 \
+  --fockian ~/data-sets/water/h2o_90.F_DIIS \
+  --density ~/data-sets/water/h2o_90.OrthoD \
+  --Ne 900 \
+  --iterations 100
+
+./spamm-charm \
+  --block 2750 \
+  --basic 2750 \
+  --tolerance 0 \
+  --operation SP2 \
+  --fockian ~/data-sets/water/h2o_110.F_DIIS \
+  --density ~/data-sets/water/h2o_110.OrthoD \
+  --Ne 1100 \
+  --iterations 100
+
+./spamm-charm \
+  --block 3250 \
+  --basic 3250 \
+  --tolerance 0 \
+  --operation SP2 \
+  --fockian ~/data-sets/water/h2o_130.F_DIIS \
+  --density ~/data-sets/water/h2o_130.OrthoD \
+  --Ne 1300 \
+  --iterations 100
+
+./spamm-charm \
+  --block 3750 \
+  --basic 3750 \
+  --tolerance 0 \
+  --operation SP2 \
+  --fockian ~/data-sets/water/h2o_150.F_DIIS \
+  --density ~/data-sets/water/h2o_150.OrthoD \
+  --Ne 1500 \
+  --iterations 100
