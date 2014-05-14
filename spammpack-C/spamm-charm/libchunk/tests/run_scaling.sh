4ad58bbb1559764c37b5339ebebc72c7b29c425c
Nicolas Bock <nicolasbock@gmail.com>
2014-05-14 16:05:26 -0600

    Another update to the OpenMP scripts.

--- spammpack-C/spamm-charm/libchunk/tests/run_scaling.sh
+++ spammpack-C/spamm-charm/libchunk/tests/run_scaling.sh
@@ -2,15 +2,16 @@
 
 NUM_THREADS=( 48 24 12 1 2 4 8 16 20 28 32 36 40 44 )
 #NUM_THREADS=( 40 44 )
-TOL=( 1.0e-10 1.0e-8 1.0e-6 )
+TOL=( 0 1.0e-10 1.0e-8 1.0e-6 )
 
 #MATRIX=h2o_30.OrthoD
 MATRIX=h2o_90.OrthoD
 
 for tol in ${TOL[@]}; do
+  echo "T = {}"
   echo "tolerance = ${tol}"
   for t in ${NUM_THREADS[@]}; do
-    echo -n "$t numpy.mean([ "
+    echo -n "T[$t] = numpy.mean([ "
     for (( i = 0; i < 5; i++ )); do
       OMP_NUM_THREADS=${t} \
         numactl --membind=0 \
--- spammpack-C/spamm-charm/libchunk/tests/run_scaling_mic.sh
+++ spammpack-C/spamm-charm/libchunk/tests/run_scaling_mic.sh
@@ -11,8 +11,8 @@ NUM_THREADS=( 240 120 60 30 1 )
 #NUM_THREADS=( 40 44 )
 TOL=( 1.0e-10 1.0e-8 1.0e-6 )
 
-MATRIX=h2o_30.OrthoD
-#MATRIX=h2o_90.OrthoD
+#MATRIX=h2o_30.OrthoD
+MATRIX=h2o_90.OrthoD
 
 # Test with full output.
 ssh $MIC "cd $MIC_DIR; LD_LIBRARY_PATH=${MIC_DIR} ./chunk_multiply --type BCSR --bcsr ${MATRIX} --tolerance 0.0e0 --no-verify"
@@ -20,7 +20,7 @@ ssh $MIC "cd $MIC_DIR; LD_LIBRARY_PATH=${MIC_DIR} ./chunk_multiply --type BCSR -
 for tol in ${TOL[@]}; do
   echo "tolerance = ${tol}"
   for t in ${NUM_THREADS[@]}; do
-    echo -n "$t numpy.mean([ "
+    echo -n "$t; numpy.mean([ "
     for (( i = 0; i < 5; i++ )); do
       ssh $MIC "cd $MIC_DIR; LD_LIBRARY_PATH=${MIC_DIR} OMP_NUM_THREADS=${t} ./chunk_multiply --type BCSR --bcsr ${MATRIX} --tolerance ${tol} --no-verify" \
         | grep seconds \
