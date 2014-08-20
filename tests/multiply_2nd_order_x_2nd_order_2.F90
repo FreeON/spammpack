de402b78160583432d0cf66f4d73029158ec1139
Nicolas Bock <nbock@lanl.gov>
2014-08-20 14:11:14 -0600

    Added new test, multiple multiplies.
    Still to do:
    
    The test should use sparse matrices and check the norm and other tree
    properties to see whether the trees are properly updated in between
    multiplications.

---
+++ tests/multiply_2nd_order_x_2nd_order_2.F90
@@ -0,0 +1,39 @@
+program test
+
+  use spammpack
+  use test_utilities
+
+  integer, parameter :: M = 12
+  integer, parameter :: N = 12
+
+  integer, parameter :: iterations = 3
+
+  type(spamm_matrix_2nd_order), pointer :: A, B, C
+  real(kind(0d0)), dimension(M, N) :: A_dense, C_dense
+  integer :: i, j
+
+  call random_number(A_dense)
+
+  A => spamm_convert_dense_to_matrix_2nd_order(A_dense)
+  B => A
+  C => spamm_zero_matrix(size(A_dense, 1), size(A_dense, 2))
+
+  do i = 1, iterations
+    call multiply(A, B, C)
+    call copy(C, A)
+    C_dense = matmul(A_dense, A_dense)
+    A_dense = C_dense
+  enddo
+
+  do i = 1, size(C_dense, 1)
+    do j = 1, size(C_dense, 2)
+      if(abs((C_dense(i, j)-get(C, i, j))/C_dense(i, j)) > 1d-10) then
+        write(*, *) "matrix element mismatch"
+        write(*, "(A,I3,A,I3,A,ES11.4)") "C_reference(", i, ",", j, ") = ", C_dense(i, j)
+        write(*, "(A,I3,A,I3,A,ES11.4)") "          C(", i, ",", j, ") = ", get(C, i, j)
+        error stop
+      endif
+    enddo
+  enddo
+
+end program test
