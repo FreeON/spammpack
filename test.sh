7791cb0c5d2d04a3a483b73f633a3a8cc269efcc
Nicolas Bock <nicolasbock@freeon.org>
2014-09-03 16:09:35 -0600

    Small CMake updates.

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-FC=gfortran CC=gcc CMAKE_BUILD_TYPE=Debug ./tester.sh
+FC=gfortran CC=gcc CMAKE_BUILD_TYPE=Release ./tester.sh || exit
+FC=gfortran CC=gcc CMAKE_BUILD_TYPE=Debug ./tester.sh || exit
 
 . /opt/intel/bin/compilervars.sh intel64
-FC=ifort CC=icc CMAKE_BUILD_TYPE=Debug ./tester.sh
+FC=ifort CC=icc CMAKE_BUILD_TYPE=Release ./tester.sh || exit
+FC=ifort CC=icc CMAKE_BUILD_TYPE=Debug ./tester.sh || exit
