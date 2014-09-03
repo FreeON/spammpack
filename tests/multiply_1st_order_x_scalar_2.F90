3fe9cb39cff1f550bc73c622d6ea667dff57daa1
Nicolas Bock <nicolasbock@freeon.org>
2014-09-03 13:55:02 -0600

    Added sparse vector unit test.

---
+++ tests/multiply_1st_order_x_scalar_2.F90
@@ -0,0 +1,35 @@
+program test
+
+#include "spamm_utility_macros.h"
+
+  use spammpack
+
+  implicit none
+
+  integer, parameter :: N = 102
+  real(kind(0d0)), parameter :: alpha = 1.2
+
+  type(spamm_matrix_order_1), pointer :: V => null()
+  real(kind(0d0)), dimension(N/10) :: V_temp
+  real(kind(0d0)), dimension(N) :: V_dense
+  integer :: i
+
+  call random_number(V_temp)
+  V_dense = 0
+  V_dense(1:size(V_temp)) = V_temp
+
+  V => spamm_convert_dense_to_order_1(V_dense)
+
+  call multiply(V, alpha)
+  V_dense = alpha*V_dense
+
+  do i = 1, N
+    if(abs(V_dense(i)-get(V, i)) > 1d-10) then
+      LOG_FATAL("vector element mismatch")
+      LOG_FATAL("V_reference("//to_string(i)//") = "//to_string(V_dense(i)))
+      LOG_FATAL("          V("//to_string(i)//") = "//to_string(get(V, i)))
+      error stop
+    endif
+  enddo
+
+end program test
--- tests/multiply_2nd_order_x_1st_order.F90
+++ tests/multiply_2nd_order_x_1st_order.F90
@@ -1,5 +1,24 @@
 program test
 
+  use spammpack
+
+  implicit none
+
+  integer, parameter :: M = 102
+  integer, parameter :: N = 78
+
+  type(spamm_matrix_2nd_order), pointer :: A => null()
+  type(spamm_matrix_order_1), pointer :: V => null()
+
+  real(kind(0d0)), dimension(M, N) :: A_dense
+  real(kind(0d0)), dimension(M) :: V_dense
+
+  call random_number(A_dense)
+  call random_number(V_dense)
+
+  A => spamm_convert_dense_to_matrix_2nd_order(A_dense)
+  V => spamm_convert_dense_to_order_1(V_dense)
+
   error stop
 
 end program test
