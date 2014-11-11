0a9b0e53f89e0091e981ff25bc0ea958061f94ea
Nicolas Bock <nicolasbock@freeon.org>
2014-11-11 13:44:21 -0700

    Updated web link to project.

--- src/spammpack.F90
+++ src/spammpack.F90
@@ -3,7 +3,8 @@
 !! The Sparse Approximate Matrix Multiply Package (SpAMMPack) is a library for
 !! approximation multiplication of sparse matrices.
 !!
-!! The library can be used by loading the main module @ref spammpack which includes the following sub modules:
+!! The library can be used by loading the main module @ref spammpack which
+!! includes the following sub modules:
 !!
 !! - @ref spamm_algebra
 !! - @ref spamm_convert
@@ -20,8 +21,9 @@
 !! USE spammpack
 !! @endcode
 !!
-!! See the documentation of @ref spammpack for more details. The spammpack project can also be found online at
-!! http://www.freeon.org/ and https://github.com/FreeON/spammpack.
+!! See the documentation of @ref spammpack for more details. The spammpack
+!! project can also be found online at http://www.freeon.org/spammpack and
+!! https://github.com/FreeON/spammpack.
 !!
 !! @copyright
 !!
@@ -31,27 +33,28 @@
 !! Redistribution and use in source and binary forms, with or without
 !! modification, are permitted provided that the following conditions are met:
 !!
-!! 1. Redistributions of source code must retain the above copyright notice, this
-!! list of conditions and the following disclaimer.
+!! 1. Redistributions of source code must retain the above copyright notice,
+!! this list of conditions and the following disclaimer.
 !!
-!! 2. Redistributions in binary form must reproduce the above copyright notice,
-!! this list of conditions and the following disclaimer in the documentation
-!! and/or other materials provided with the distribution.
+!! 2. Redistributions in binary form must reproduce the above copyright
+!! notice, this list of conditions and the following disclaimer in the
+!! documentation and/or other materials provided with the distribution.
 !!
-!! 3. Neither the name of the copyright holder nor the names of its contributors
-!! may be used to endorse or promote products derived from this software without
-!! specific prior written permission.
+!! 3. Neither the name of the copyright holder nor the names of its
+!! contributors may be used to endorse or promote products derived from this
+!! software without specific prior written permission.
 !!
 !! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 !! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-!! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-!! DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-!! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-!! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-!! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-!! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-!! OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-!! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+!! ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+!! LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+!! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+!! SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+!! INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+!! CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+!! ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+!! POSSIBILITY OF SUCH DAMAGE.
 !!
 !! @author Matt Challacombe matt.challacombe@freeon.org
 !! @author Nicolas Bock nicolasbock@freeon.org
