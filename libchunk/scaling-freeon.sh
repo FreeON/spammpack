e0bd38f2ce1b62d81105e32374c81a8a94b2916e
Nicolas Bock <nicolasbock@freeon.org>
2015-01-13 13:35:06 -0700

    Removing chunk code.

--- libchunk/scaling-freeon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./configure-freeon.sh parallel
-make clean && make
-bash -c "cd tests && ./scaling-freeon-dense.sh"
-
-./configure-freeon.sh sequential
-make clean && make
-bash -c "cd tests && ./scaling-freeon-SpAMM.sh"
--- libchunk/tests/scaling_OpenMP.gnuplot
+++
@@ -1,6 +0,0 @@
-# vim: syntax=gnuplot
-
-set terminal postscript color
-set output 'scaling_with_tolerance.ps'
-
-plot '< ./process_scaling_data.py --x-axis tolerance scaling_OpenMP.dat' index 0 using 1:2
