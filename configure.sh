7c79dad7926d278114fb4a94e3a26bb113884167
Nicolas Bock <nicolasbock@freeon.org>
2015-01-21 11:16:08 -0700

    Update configure.sh convenience script.

--- configure.sh
+++ configure.sh
@@ -2,9 +2,14 @@
 
 git clean -df
 
-cmake . \
+mkdir build
+cd build
+
+cmake .. \
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} \
-  -DDEBUG_LEVEL=2 \
+  -DSPAMM_DEBUG_LEVEL=${SPAMM_DEBUG_LEVEL:-2} \
   -DCMAKE_VERBOSE_MAKEFILE=yes \
   -DCMAKE_C_COMPILER=${CC:-gcc} \
   -DCMAKE_Fortran_COMPILER=${FC:-gfortran} || exit
+
+echo "The sources are configured in the build directory."
