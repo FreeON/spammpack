32514a2e79fd6c4e45fe063baf0d2c787eddbbe3
Nicolas Bock <nicolasbock@freeon.org>
2015-02-27 16:52:11 -0700

    Checkpoint.

--- tests-chunk/add_chunk_2d.F90
+++ tests-chunk/add_chunk_2d.F90
@@ -5,5 +5,12 @@ program test
   implicit none
 
   type(chunk_2d), pointer :: A, B
+  real(kind(0d0)) :: A_dense(SPAMM_CHUNK_SIZE, SPAMM_CHUNK_SIZE)
+  real(kind(0d0)) :: B_dense(SPAMM_CHUNK_SIZE, SPAMM_CHUNK_SIZE)
+
+  allocate(A)
+  call init_2d_random(A)
+
+  B => chunk_add_2d_2d(A, A)
 
 end program test
