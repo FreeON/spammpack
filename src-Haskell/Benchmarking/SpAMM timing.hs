d420d0f98d4ea20c0b7bcc204a2e68dc913ea7d2
Eric Chisolm <eric.chisolm@gmail.com>
2015-03-08 20:29:01 -0600

    Improved scripts for benchmarking

--- src-Haskell/Benchmarking/SpAMM timing.hs
+++ src-Haskell/Benchmarking/SpAMM timing.hs
@@ -1,26 +1,24 @@
-import MatrixTree ; import SpAMM ; import System.CPUTime ; import System.Directory (removeFile)
-import System.IO (hClose, openTempFile)
+import MatrixTree ; import SpAMM
+import System.CPUTime
+import System.Random (getStdGen, newStdGen, randomRs, StdGen)
 
-main = do let sizes = fmap (2^) [11..20]
+main = do let sizes = fmap (2^) [1..20]
           putStrLn "treeAdd"
           mapM_ (doTiming treeAdd) sizes
           putStrLn "treeMult"
           mapM_ (doTiming treeMult) sizes
 
 doTiming :: (MatrixTree -> MatrixTree -> MatrixTree) -> Int -> IO ()
-doTiming op size = do let fstTree = makeRandomTree size ; sndTree = makeRandomTree size
+doTiming op size = do gen <- getStdGen ; let fstTree = makeRandomTree size gen
+                      gen <- newStdGen ; let sndTree = makeRandomTree size gen
                       t1 <- getCPUTime
                       tree <- return $ op fstTree sndTree
                       t2 <- getCPUTime
-                      (tempName, tempHandle) <- openTempFile "." "temp"
-                      hClose tempHandle
-                      removeFile tempName
-                      writeTreeToMatrixMarket tree "coordinate" tempName
-                      removeFile tempName
                       let time = fromIntegral (t2 - t1) * 1e-12 :: Double
-                      putStrLn $ show size ++ " " ++ show time
+                      putStrLn $ show (norm tree) ++ " " ++ show size ++ " " ++ show time
 
-makeRandomTree :: Int -> MatrixTree
-makeRandomTree size = indexedListToTree (size, size, ijxs)
-                      where ijxs = [(i, j, x) | j <- [1..size], i <- [1..size], x <- randomNums]
-                            randomNums = take (size^2) $ randomRs (0.0, 1.0) (mkStdGen 245)
+makeRandomTree :: Int -> StdGen -> MatrixTree
+makeRandomTree size gen = indexedListToTree (size, size, ijxs)
+                          where ijxs = zipWith (\(i, j) x -> (i, j, x)) indices randomNums
+                                indices = [(i, j) | j <- [1..size], i <- [1..size]]
+                                randomNums = take (size^2) $ randomRs (0.0, 1.0) gen
--- src-Haskell/Benchmarking/norm timing.hs
+++ src-Haskell/Benchmarking/norm timing.hs
@@ -1,18 +1,21 @@
-import MatrixTree ; import System.CPUTime ; import System.Random (mkStdGen, randomRs)
+import MatrixTree
+import System.CPUTime
+import System.Random (getStdGen, randomRs, StdGen)
 
-main = do let sizes = fmap (2^) [11..20]
+main = do let sizes = fmap (2^) [1..20]
           putStrLn "norm"
           mapM_ (doTiming setNorm) sizes
 
 doTiming :: (MatrixTree -> Norm) -> Int -> IO ()
-doTiming op size = do let tree = makeRandomTree size
+doTiming op size = do gen <- getStdGen ; let tree = makeRandomTree size gen
                       t1 <- getCPUTime
                       norm <- return $ op tree
                       t2 <- getCPUTime
                       let time = fromIntegral (t2 - t1) * 1e-12 :: Double
                       putStrLn $ show norm ++ " " ++ show size ++ " " ++ show time
 
-makeRandomTree :: Int -> MatrixTree
-makeRandomTree size = indexedListToTree (size, size, ijxs)
-                      where ijxs = [(i, j, x) | j <- [1..size], i <- [1..size], x <- randomNums]
-                            randomNums = take (size^2) $ randomRs (0.0, 1.0) (mkStdGen 245)
+makeRandomTree :: Int -> StdGen -> MatrixTree
+makeRandomTree size gen = indexedListToTree (size, size, ijxs)
+                          where ijxs = zipWith (\(i, j) x -> (i, j, x)) indices randomNums
+                                indices = [(i, j) | j <- [1..size], i <- [1..size]]
+                                randomNums = take (size^2) $ randomRs (0.0, 1.0) gen
