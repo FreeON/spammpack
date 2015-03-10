25ae6700994504ccb5899d0a494b898afcde4a98
Eric Chisolm <eric.chisolm@gmail.com>
2015-03-09 21:10:21 -0600

    Norm and SpAMM timing tests now work

--- src-Haskell/Benchmarking/SpAMM timing.hs
+++ src-Haskell/Benchmarking/SpAMM timing.hs
@@ -1,24 +1,34 @@
-import MatrixTree ; import SpAMM
-import System.CPUTime
+import Data.List (intersperse)
+import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import MatrixTree (indexedListToTree, MatrixTree, Norm, norm, setNorm)
+import SpAMM (treeAdd, treeMult)
+import System.Directory (removeFile)
+import System.IO (Handle, hClose, hPutStr, openTempFile)
 import System.Random (getStdGen, newStdGen, randomRs, StdGen)
 
-main = do let sizes = fmap (2^) [1..20]
+main = do (tempName, tempHandle) <- openTempFile "." "temp"
+          let sizes = fmap (2^) [7..11]
           putStrLn "treeAdd"
-          mapM_ (doTiming treeAdd) sizes
+          mapM_ (doTiming tempHandle treeAdd) sizes
           putStrLn "treeMult"
-          mapM_ (doTiming treeMult) sizes
+          mapM_ (doTiming tempHandle treeMult) sizes
+          hClose tempHandle; removeFile tempName
 
-doTiming :: (MatrixTree -> MatrixTree -> MatrixTree) -> Int -> IO ()
-doTiming op size = do gen <- getStdGen ; let fstTree = makeRandomTree size gen
-                      gen <- newStdGen ; let sndTree = makeRandomTree size gen
-                      t1 <- getCPUTime
-                      tree <- return $ op fstTree sndTree
-                      t2 <- getCPUTime
-                      let time = fromIntegral (t2 - t1) * 1e-12 :: Double
-                      putStrLn $ show (norm tree) ++ " " ++ show size ++ " " ++ show time
+doTiming :: Handle -> (MatrixTree -> MatrixTree -> MatrixTree) -> Int -> IO ()
+doTiming handle op size =
+         do gen <- getStdGen ; let fstTree = makeRandomTree size gen
+            gen <- newStdGen ; let sndTree = makeRandomTree size gen
+            t1 <- getCurrentTime
+            let tree = op fstTree sndTree
+            hPutStr handle (show $ norm tree)
+            t2 <- getCurrentTime
+            printList [show size, show $ diffUTCTime t2 t1]
 
 makeRandomTree :: Int -> StdGen -> MatrixTree
 makeRandomTree size gen = indexedListToTree (size, size, ijxs)
-                          where ijxs = zipWith (\(i, j) x -> (i, j, x)) indices randomNums
-                                indices = [(i, j) | j <- [1..size], i <- [1..size]]
-                                randomNums = take (size^2) $ randomRs (0.0, 1.0) gen
+               where ijxs = zipWith (\(i, j) x -> (i, j, x)) indices randomNums
+                     indices = [(i, j) | j <- [1..size], i <- [1..size]]
+                     randomNums = take (size^2) $ randomRs (0.0, 1.0) gen
+
+printList :: [String] -> IO ()
+printList = putStrLn . concat . intersperse " "
--- src-Haskell/Benchmarking/norm timing.hs
+++ src-Haskell/Benchmarking/norm timing.hs
@@ -1,21 +1,30 @@
-import MatrixTree
-import System.CPUTime
+import Data.List (intersperse)
+import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import MatrixTree (indexedListToTree, MatrixTree, Norm, setNorm)
+import System.Directory (removeFile)
+import System.IO (Handle, hClose, hPutStr, openTempFile)
 import System.Random (getStdGen, randomRs, StdGen)
 
-main = do let sizes = fmap (2^) [1..20]
+main = do (tempName, tempHandle) <- openTempFile "." "temp"
+          let sizes = fmap (2^) [7..13]
           putStrLn "norm"
-          mapM_ (doTiming setNorm) sizes
+          mapM_ (doTiming tempHandle setNorm) sizes
+          hClose tempHandle; removeFile tempName
 
-doTiming :: (MatrixTree -> Norm) -> Int -> IO ()
-doTiming op size = do gen <- getStdGen ; let tree = makeRandomTree size gen
-                      t1 <- getCPUTime
-                      norm <- return $ op tree
-                      t2 <- getCPUTime
-                      let time = fromIntegral (t2 - t1) * 1e-12 :: Double
-                      putStrLn $ show norm ++ " " ++ show size ++ " " ++ show time
+doTiming :: Handle -> (MatrixTree -> Norm) -> Int -> IO ()
+doTiming handle op size =
+         do gen <- getStdGen ; let tree = makeRandomTree size gen
+            t1 <- getCurrentTime
+            let norm = op tree
+            hPutStr handle $ show norm
+            t2 <- getCurrentTime
+            printList [show size, show $ diffUTCTime t2 t1]
 
 makeRandomTree :: Int -> StdGen -> MatrixTree
 makeRandomTree size gen = indexedListToTree (size, size, ijxs)
-                          where ijxs = zipWith (\(i, j) x -> (i, j, x)) indices randomNums
-                                indices = [(i, j) | j <- [1..size], i <- [1..size]]
-                                randomNums = take (size^2) $ randomRs (0.0, 1.0) gen
+               where ijxs = zipWith (\(i, j) x -> (i, j, x)) indices randomNums
+                     indices = [(i, j) | j <- [1..size], i <- [1..size]]
+                     randomNums = take (size^2) $ randomRs (0.0, 1.0) gen
+
+printList :: [String] -> IO ()
+printList = putStrLn . concat . intersperse " "
