adf621e0f932c5fb1aa386ca3497419fe503d97e
Nicolas Bock <nicolasbock@gmail.com>
2015-03-13 11:31:43 -0600

    Updated script.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo "Installing bundler and all necessary gems..."
 gem install bundler
-bundle install
+bundle install --binstubs
 
 echo "The gems can be updated by running"
 echo "bundle update"
