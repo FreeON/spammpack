6495d4375eaecda4823801a2c35750df55948f97
Nicolas Bock <nicolasbock@gmail.com>
2015-04-03 16:45:26 -0600

    Updated scaling script.

--- spammpack-C/spamm-charm/libchunk/tests/scaling-1.sh
+++ spammpack-C/spamm-charm/libchunk/tests/scaling-1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
 # Building with gcc:
-# ./configure --disable-assert --enable-chunk-tree-max-tier=4 CFLAGS=-"O3 -ftree-vectorize -g"
+pushd ..
+./configure --disable-assert --enable-chunk-tree-max-tier=4 CFLAGS=-"O3 -ftree-vectorize -g"
+make clean
+make
+popd
 
 for tolerance in 1e-10 1e-6; do
   for threads in 1 2 4 8 12 16 20 24 28 32 36 40 44 48; do
@@ -9,7 +13,7 @@ for tolerance in 1e-10 1e-6; do
       --interleave=all -- \
       ./chunk_multiply \
       --tolerance ${tolerance} \
-      --N_basic 32 \
+      --N_basic 4 \
       --type BCSR \
       --bcsr ~/data-sets/water/h2o_90.OrthoD \
       --no-verify \
