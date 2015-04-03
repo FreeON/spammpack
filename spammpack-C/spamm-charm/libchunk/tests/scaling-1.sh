cf26c13fa96ef8f6f447a0ec834c65d4fa46ebc7
Nicolas Bock <nicolasbock@gmail.com>
2015-04-03 16:15:26 -0600

    Added configure command for gcc.

--- spammpack-C/spamm-charm/libchunk/tests/scaling-1.sh
+++ spammpack-C/spamm-charm/libchunk/tests/scaling-1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Building with gcc:
+# ./configure --disable-assert --enable-chunk-tree-max-tier=4 CFLAGS=-"O3 -ftree-vectorize -g"
 
 for tolerance in 1e-10 1e-6; do
   for threads in 1 2 4 8 12 16 20 24 28 32 36 40 44 48; do
