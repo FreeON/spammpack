233cc47e1b6eff750813ba9cd4177a7000cc6460
Nicolas Bock <nicolasbock@gmail.com>
2015-04-28 06:53:51 -0600

    Fix add

--- src-chunk/spamm_chunk_add_2d.F90
+++ src-chunk/spamm_chunk_add_2d.F90
@@ -34,7 +34,7 @@
 !> Module for adding 2D chunks.
 !!
 !! \todo
-!!    - Implement adding two matrices (\f$ C \leftarrow A+B \f$).
+!!    - Implement adding two matrices: \f$ C \leftarrow A+B \f$.
 !!        - Check matching bounds.
 !!        - Update norms.
 module spamm_chunk_add_2d
@@ -47,17 +47,24 @@ contains
 
   !> Add two 2-d chunks.
   !!
-  !! \f[ C \leftarrow A + B \f]
+  !! \f[ C \leftarrow \alpha A + \beta B \f]
   !!
   !! @param A Chunk A.
   !! @param B Chunk B.
+  !! @param alpha The factor \f$ \alpha \f$.
+  !! @param beta The factor \f$ \beta \f$.
   !! @return The result.
-  function chunk_add_2d_2d (A, B) result (C)
+  function chunk_add_2d_2d (A, B, alpha, beta) result (C)
 
     use spamm_chunk_copy_2d
 
     type(chunk_2d_t), pointer :: C
     type(chunk_2d_t), pointer, intent(in) :: A, B
+    double precision, intent(in), optional :: alpha
+    double precision, intent(in), optional :: beta
+
+    double precision :: alpha_, beta_
+    integer :: i, j
 
     nullify(C)
 
@@ -80,6 +87,24 @@ contains
        error stop
     endif
 
+    if(present(alpha)) then
+       alpha_ = alpha
+    else
+       alpha_ = 1
+    endif
+
+    if(present(beta)) then
+       beta_ = beta
+    else
+       beta_ = 1
+    endif
+
+    allocate(C)
+    do i = 1, SPAMM_CHUNK_BLOCKS
+       do j = 1, SPAMM_CHUNK_BLOCKS
+          C%data(i, j)%data = alpha_*A%data(i, j)%data+beta_*B%data(i, j)%data
+       enddo
+    enddo
 
   end function chunk_add_2d_2d
 
--- tests-chunk/add_chunk_2d.F90
+++ tests-chunk/add_chunk_2d.F90
@@ -4,6 +4,9 @@ program test
 
   implicit none
 
+  double precision, parameter :: alpha = 1.2
+  double precision, parameter :: beta = 0.8
+
   type(chunk_2d_t), pointer :: A, B, C
   double precision :: A_dense(SPAMM_CHUNK_SIZE, SPAMM_CHUNK_SIZE)
   double precision :: B_dense(SPAMM_CHUNK_SIZE, SPAMM_CHUNK_SIZE)
@@ -16,19 +19,22 @@ program test
   call init_2d_random(B)
 
   nullify(C)
-  C => chunk_add_2d_2d(A, B)
+  C => chunk_add_2d_2d(A, B, alpha, beta)
 
   A_dense = chunk_2d_to_dense(A)
   B_dense = chunk_2d_to_dense(B)
+  C_dense = chunk_2d_to_dense(C)
 
   do i = 1, SPAMM_CHUNK_SIZE
      do j = 1, SPAMM_CHUNK_SIZE
-        if(abs(A_dense(i, j)-B_dense(i, j)) > 1e-12) then
+        if(abs(alpha*A_dense(i, j)+beta*B_dense(i, j)-C_dense(i, j)) > 1e-12) then
            write(*, *) "matrix element mismatch"
            write(*, *) trim(chunk_2d_to_string(A))
            write(*, *) trim(chunk_2d_to_string(B))
+           write(*, *) trim(chunk_2d_to_string(C))
            write(*, *) A_dense
            write(*, *) B_dense
+           write(*, *) C_dense
            error stop
         endif
      enddo
