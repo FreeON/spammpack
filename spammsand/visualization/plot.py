9daccd212f34e271b5a6432042a1c577b7e33e09
Nicolas Bock <nicolasbock@freeon.org>
2015-07-21 16:43:40 -0600

    Add new mayavi script
    The script can be run on the vtk data files generated by the inverse sqrt
    program, as:
    
    $ python2 spammsand/visualization/plot.py z_stab5.vtk

---
+++ spammsand/visualization/plot.py
@@ -0,0 +1,17 @@
+from mayavi import mlab
+from mayavi import tools
+
+@mlab.show
+def main(vtk_file):
+    plot=tools.pipeline.open(vtk_file)
+    tools.pipeline.glyph(plot, scale_factor=0.95, mode='cube')
+    mlab.show()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "VTK",
+        help="The VTK data file")
+    options = parser.parse_args()
+    main(options.VTK)
