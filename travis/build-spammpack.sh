34667bc5b5bbba971521328da30492ca9c6acd41
Nicolas Bock <nicolasbock@freeon.org>
2015-08-18 12:32:57 -0600

    Fix Travis

--- travis/build-spammpack.sh
+++ travis/build-spammpack.sh
@@ -4,7 +4,9 @@ cmake . \
   -DCMAKE_INSTALL_PREFIX=${PWD}/install \
   -DCMAKE_Fortran_COMPILER=gfortran-${GCC_VERSION} \
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-  -DCMAKE_VERBOSE_MAKEFILE=yes || exit
+  -DCMAKE_VERBOSE_MAKEFILE=yes \
+  -DSPAMM_TESTING=yes \
+  || exit
 make || exit
 make install || exit
 ctest --verbose || exit
